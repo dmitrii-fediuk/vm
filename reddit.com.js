1e792f73949f367afe482b20cee5009f2d799997
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-17 21:39:57 +0100

    ⠀

--- reddit.com.js
+++ reddit.com.js
@@ -3,7 +3,7 @@
 // 2) https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/metadata-block.md#repos-sticky-header
 // ==UserScript==
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
-// @description 5/17/2024, 10:57:34 PM
+// @description https://github.com/dmitrii-fediuk/vm/blob/main/reddit.com.js
 // @grant none
 // 2024-05-17
 // 1.1) https://violentmonkey.github.io/api/metadata-block#match--exclude-match
