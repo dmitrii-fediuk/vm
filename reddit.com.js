bd0c31506eb29f8c27017c5152cfed8d896ff36d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-17 21:30:51 +0100

    ⠀

--- reddit.com.js
+++ reddit.com.js
@@ -4,17 +4,17 @@
 // ==UserScript==
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @description 5/17/2024, 10:57:34 PM
-// @grant       none
+// @grant none
 // 2024-05-17
 // 1.1) https://violentmonkey.github.io/api/metadata-block#match--exclude-match
 // 1.2) https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/metadata-block.md#match--exclude-match
 // 2.1) https://violentmonkey.github.io/api/matching
 // 2.2) https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/matching.md#repos-sticky-header
-// @match       https://www.reddit.com/*
+// @match https://www.reddit.com/*
 // 2024-05-17
 // 1) https://violentmonkey.github.io/api/metadata-block#name
 // 2) https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/metadata-block.md#name
-// @name        reddit.com
-// @version     0.0.1
+// @name reddit.com
+// @version 0.0.1
 // ==/UserScript==
 console.log('TEST 2');
