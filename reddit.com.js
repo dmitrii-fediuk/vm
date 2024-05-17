29bd028025d3c92fa92048ecb3825079407e8266
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-17 21:41:50 +0100

    ⠀

--- reddit.com.js
+++ reddit.com.js
@@ -5,6 +5,8 @@
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @grant none
 // 2024-05-17
+// @icon https://www.redditstatic.com/shreddit/assets/favicon/64x64.png
+// 2024-05-17
 // 1.1) https://violentmonkey.github.io/api/metadata-block#match--exclude-match
 // 1.2) https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/metadata-block.md#match--exclude-match
 // 2.1) https://violentmonkey.github.io/api/matching
