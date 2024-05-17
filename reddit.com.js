a742e7def6a91b0bf153a82125543147c73f5c64
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-17 20:58:01 +0100

    ⠀

---
+++ reddit.com.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        New script reddit.com
+// @namespace   Violentmonkey Scripts
+// @match       https://www.reddit.com/r/Bedding/comments/xkmrtq/is_supima_cotton_all_hype_or_actually_a_real_thing/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 5/17/2024, 10:57:34 PM
+// ==/UserScript==
+console.log('TEST');
