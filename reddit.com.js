ee208c3ca12b15c46f624fe6e744af56a8d52142
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-17 21:35:33 +0100

    ⠀

--- reddit.com.js
+++ reddit.com.js
@@ -11,7 +11,7 @@
 // 2.1) https://violentmonkey.github.io/api/matching
 // 2.2) https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/matching.md#repos-sticky-header
 // 3) https://developer.chrome.com/docs/extensions/develop/concepts/match-patterns#gc-wrapper
-// @match https://www.reddit.com/*
+// @match *://*.reddit.com/*
 // 2024-05-17
 // 1) https://violentmonkey.github.io/api/metadata-block#name
 // 2) https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/metadata-block.md#name
