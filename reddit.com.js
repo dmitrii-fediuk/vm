fee22ff80917ae2e1da82f6f5764cc70ea6731bd
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-17 21:28:11 +0100

    ⠀

--- reddit.com.js
+++ reddit.com.js
@@ -1,10 +1,15 @@
 // 2024-05-17
-// https://violentmonkey.github.io/api/metadata-block
-// https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/metadata-block.md#repos-sticky-header
+// 1) https://violentmonkey.github.io/api/metadata-block
+// 2) https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/metadata-block.md#repos-sticky-header
 // ==UserScript==
 // @author      Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @description 5/17/2024, 10:57:34 PM
 // @grant       none
+// 2024-05-17
+// 1.1) https://violentmonkey.github.io/api/metadata-block#match--exclude-match
+// 1.2) https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/metadata-block.md#match--exclude-match
+// 2.1) https://violentmonkey.github.io/api/matching
+// 2.2) https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/matching.md#repos-sticky-header
 // @match       https://www.reddit.com/*
 // 2024-05-17
 // 1) https://violentmonkey.github.io/api/metadata-block#name
