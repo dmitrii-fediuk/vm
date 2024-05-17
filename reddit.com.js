5e4ca1a8b1539ca76b1800427f0f10abe0485fbb
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-17 21:23:00 +0100

    ⠀

--- reddit.com.js
+++ reddit.com.js
@@ -1,14 +1,16 @@
+// 2024-05-17
+// https://violentmonkey.github.io/api/metadata-block
+// https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/metadata-block.md#repos-sticky-header
 // ==UserScript==
 // @author      Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @description 5/17/2024, 10:57:34 PM
 // @grant       none
 // @match       https://www.reddit.com/*
-// 2024-05-17 https://violentmonkey.github.io/api/metadata-block#name
+// 2024-05-17
+// 1) https://violentmonkey.github.io/api/metadata-block#name
+// 2) https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/metadata-block.md#name
 // @name        reddit.com
 // @namespace   Dmitrii Fediuk
 // @version     0.0.1
 // ==/UserScript==
-// 2024-05-17
-// https://violentmonkey.github.io/api/metadata-block
-// https://github.com/violentmonkey/violentmonkey.github.io/blob/587d6569/content/api/metadata-block.md#repos-sticky-header
 console.log('TEST 2');
