be637320767d329dd0d179b56c0a471eb92ed933
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-30 11:23:27 +0100

    https://github.com/dmitrii-fediuk/adblock/issues/33

--- s/pikabu.ru.js
+++ s/pikabu.ru.js
@@ -1,4 +1,3 @@
-// 2024-05-30 "Hide the right sidebar from pikabu.ru": https://github.com/dmitrii-fediuk/adblock/issues/32
 // ==UserScript==
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @grant GM_addStyle
@@ -7,5 +6,9 @@
 // @match *://pikabu.ru/*
 // @name pikabu.ru
 // ==/UserScript==
+// 2024-05-30 "Hide the right sidebar from pikabu.ru": https://github.com/dmitrii-fediuk/adblock/issues/32
 // language=CSS
 GM_addStyle('.main {float: none !important; padding: 0 !important; width: 100% !important;}');
+// 2024-05-30 "Hide the left sidebar from pikabu.ru": https://github.com/dmitrii-fediuk/adblock/issues/32
+// language=CSS
+GM_addStyle('.app__inner {padding: 0 !important;}');
