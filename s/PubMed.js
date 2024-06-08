db00096360d40371ee30deac3ec33002db4ff5fc
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-08 08:41:18 +0100

    https://github.com/dmitrii-fediuk/vm/issues/1

--- s/PubMed.js
+++ s/PubMed.js
@@ -18,3 +18,5 @@ GM_addStyle('@media screen and (min-width: 1020px) {.search-form > .inner-wrap {
 GM_addStyle('.search-page > .inner-wrap {width: initial;}');
 // language=CSS
 GM_addStyle('#search-results {width: calc(100% - 260px);}');
+// language=CSS
+GM_addStyle('.article-page #abstract {margin: 0 !important;}');
