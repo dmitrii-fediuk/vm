d2c996474fc0fcb71d72076bd266071bd60fe81d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-22 05:04:59 +0100

    https://github.com/dmitrii-fediuk/vm/issues/3

--- s/data.tuik.gov.tr.js
+++ s/data.tuik.gov.tr.js
@@ -3,7 +3,7 @@
 // @grant GM_addStyle
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/data.tuik.gov.tr.js
 // @icon https://data.tuik.gov.tr/img/favicon.ico
-// @match *://data.tuik.gov.tr/Search/Search/*
+// @match *://data.tuik.gov.tr/Search/Search?*
 // @name Turkish Statistical Institute
 // ==/UserScript==
 // 2024-06-22 "Remove the header from the search result pages of the Turkish Statistical Institute website (`data.tuik.gov.tr`)":
