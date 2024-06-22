c7941895c3fd91ccae77e610e892459ec6137d5c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-22 05:00:44 +0100

    https://github.com/dmitrii-fediuk/vm/issues/3

---
+++ s/data.tuik.gov.tr.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/data.tuik.gov.tr.js
+// @icon https://data.tuik.gov.tr/img/favicon.ico
+// @match *://data.tuik.gov.tr/Search/Search/*
+// @name Turkish Statistical Institute
+// ==/UserScript==
