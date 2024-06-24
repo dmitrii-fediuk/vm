7e4d6d0279e7d9d073d46e61b18f76c70bfc64ab
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-24 04:34:49 +0100

    https://github.com/dmitrii-fediuk/vm/issues/6

--- s/club.dns-shop.ru.js
+++ s/club.dns-shop.ru.js
@@ -9,3 +9,6 @@
 // 2024-06-24 "Improve `club.dns-shop.ru`": https://github.com/dmitrii-fediuk/vm/issues/6
 // language=CSS
 GM_addStyle('.post-page__content {max-width: initial;}');
+// language=CSS
+GM_addStyle('.sidebar-space {display: none;}');
+
