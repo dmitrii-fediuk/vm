c2be144baf8b0c1d4943298a4de3f36cce50331b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-24 04:29:35 +0100

    https://github.com/dmitrii-fediuk/vm/issues/6

---
+++ s/club.dns-shop.ru.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/club.dns-shop.ru.js
+// @icon https://data.tuik.gov.tr/img/favicon.ico
+// @match *://club.dns-shop.ru/*
+// @name club.dns-shop.ru
+// ==/UserScript==
+// 2024-06-24 "Improve `club.dns-shop.ru`": https://github.com/dmitrii-fediuk/vm/issues/6
+// language=CSS
+GM_addStyle('header, .bg-gradHeader {display: none;}');
