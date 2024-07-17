cfb78da27cf48a66c60721b42c7fb095a7117d8a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-07-17 11:12:12 +0100

    https://github.com/dmitrii-fediuk/vm/issues/10

--- s/digitalocean.com.js
+++ s/digitalocean.com.js
@@ -3,7 +3,7 @@
 // @grant GM_addStyle
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/digitalocean.com.js
 // @icon https://www.digitalocean.com/_next/static/media/apple-touch-icon.d7edaa01.png
-// @match *://digitalocean.com/*
+// @match *://www.digitalocean.com/*
 // @name digitalocean.com
 // ==/UserScript==
 // 2024-07-17 "Prevent the header from sticking on `digitalocean.com`": https://github.com/dmitrii-fediuk/vm/issues/10
