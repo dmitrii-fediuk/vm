30cc162321e2749d7160ba835a62721ce1f2bc4f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-07-17 11:04:02 +0100

    https://github.com/dmitrii-fediuk/vm/issues/10

--- s/digitalocean.com.js
+++ s/digitalocean.com.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/digitalocean.com.js
 // @icon https://www.digitalocean.com/_next/static/media/apple-touch-icon.d7edaa01.png
 // @match *://digitalocean.com/*
-// @name openvpn.net
+// @name digitalocean.com
 // ==/UserScript==
 // 2024-07-17 "Prevent the header from sticking on `digitalocean.com`": https://github.com/dmitrii-fediuk/vm/issues/10
 // language=CSS
