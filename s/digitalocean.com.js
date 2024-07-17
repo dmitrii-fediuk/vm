19db99426ede43697e5f0efd5a9809aed76f2d48
Dmitrii Fediuk <dfediuk@gmail.com>
2024-07-17 11:03:28 +0100

    https://github.com/dmitrii-fediuk/vm/issues/10

---
+++ s/digitalocean.com.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/digitalocean.com.js
+// @icon https://www.digitalocean.com/_next/static/media/apple-touch-icon.d7edaa01.png
+// @match *://digitalocean.com/*
+// @name openvpn.net
+// ==/UserScript==
+// 2024-07-17 "Prevent the header from sticking on `digitalocean.com`": https://github.com/dmitrii-fediuk/vm/issues/10
+// language=CSS
+GM_addStyle('[class^="HeaderStyles__StyledHeader"] {position: absolute !important;}');
