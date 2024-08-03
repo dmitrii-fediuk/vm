6c2ab4972e10a37d0c71eb09818705a2b3e1ce93
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-03 06:01:03 +0100

    https://github.com/dmitrii-fediuk/adblock/issues/77

--- s/support.weltpixel.com.js
+++ s/support.weltpixel.com.js
@@ -6,6 +6,9 @@
 // @match *://support.weltpixel.com/*
 // @name support.weltpixel.com
 // ==/UserScript==
+// 2024-08-03 "Hide the left sidebar from `support.weltpixel.com`": https://github.com/dmitrii-fediuk/adblock/issues/77
+// language=CSS
+GM_addStyle('.article {max-width: 100% !important;}');
 // 2024-08-03 "Hide the top advertisements from `support.weltpixel.com`": https://github.com/dmitrii-fediuk/vm/issues/11
 // language=CSS
 GM_addStyle('.hc_header > a {display: none !important;}');
