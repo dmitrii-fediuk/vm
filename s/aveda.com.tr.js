acb8b265ccbdf69551665d282ae3761991893f5f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-06 19:06:25 +0100

    https://github.com/dmitrii-fediuk/vm/issues/15

--- s/aveda.com.tr.js
+++ s/aveda.com.tr.js
@@ -3,12 +3,15 @@
 // @grant GM_addStyle
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/aveda.com.tr.js
 // @icon https://www.aveda.com.tr/sites/aveda/themes/aveda_base/favicon.ico
+// @match *://www.aveda.com/*
 // @match *://www.aveda.com.tr/*
 // @name aveda.com.tr
 // ==/UserScript==
 // 2024-08-06 "Prevent the header from sticking on `aveda.com.tr`": https://github.com/dmitrii-fediuk/vm/issues/12
 // language=CSS
 GM_addStyle('.site-header__main {position: initial !important;}');
-// 2024-08-06 "Prevent the footer from sticking on `aveda.com.tr`": https://github.com/dmitrii-fediuk/vm/issues/13
+// 2024-08-06
+// 1) "Prevent the footer from sticking on `aveda.com.tr`": https://github.com/dmitrii-fediuk/vm/issues/13
+// 2) "Prevent the footer from sticking on `aveda.com`": https://github.com/dmitrii-fediuk/vm/issues/15
 // language=CSS
-GM_addStyle('.footer--sticky.is-sticky {position: initial !important;}');
+GM_addStyle('.is-sticky {position: initial !important;}');
