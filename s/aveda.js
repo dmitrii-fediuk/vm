3d504edc1d2379675b0fbac2499f674a9c84f728
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-06 19:08:29 +0100

    refactoring

--- s/aveda.js
+++ s/aveda.js
@@ -8,12 +8,12 @@
 // @name Aveda
 // ==/UserScript==
 // 2024-08-06
-// 1) "Prevent the header from sticking on `aveda.com.tr`": https://github.com/dmitrii-fediuk/vm/issues/12
-// 2) "Prevent the header from sticking on `aveda.com`": https://github.com/dmitrii-fediuk/vm/issues/14
-// language=CSS
-GM_addStyle('.site-header__main {position: initial !important;}');
-// 2024-08-06
 // 1) "Prevent the footer from sticking on `aveda.com.tr`": https://github.com/dmitrii-fediuk/vm/issues/13
 // 2) "Prevent the footer from sticking on `aveda.com`": https://github.com/dmitrii-fediuk/vm/issues/15
 // language=CSS
 GM_addStyle('.is-sticky {position: initial !important;}');
+// 2024-08-06
+// 1) "Prevent the header from sticking on `aveda.com.tr`": https://github.com/dmitrii-fediuk/vm/issues/12
+// 2) "Prevent the header from sticking on `aveda.com`": https://github.com/dmitrii-fediuk/vm/issues/14
+// language=CSS
+GM_addStyle('.site-header__main {position: initial !important;}');
