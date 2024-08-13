669e81df378669eeddb995f98ad6a2f31f2ee5df
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-13 01:53:14 +0100

    https://github.com/dmitrii-fediuk/vm/issues/22

--- s/translated.turbopages.org.js
+++ s/translated.turbopages.org.js
@@ -2,7 +2,7 @@
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @grant GM_addStyle
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/translated.turbopages.org.js
-// @icon https://cdn.trustpilot.net/brand-assets/4.3.0/favicons/apple-touch-icon.png
+// @icon https://translate.yandex.ru/icons/favicon.png
 // @match *://translated.turbopages.org/*
 // @name translated.turbopages.org
 // ==/UserScript==
