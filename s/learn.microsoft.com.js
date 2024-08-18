07b04dbde4c2c960fb5d90226552c60ac80a0c0b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-18 11:51:12 +0100

    https://github.com/dmitrii-fediuk/vm/issues/23

--- s/learn.microsoft.com.js
+++ s/learn.microsoft.com.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/learn.microsoft.com.js
 // @icon https://learn.microsoft.com/en-us/media/open-graph-image.png
 // @match *://learn.microsoft.com/*
-// @name openvpn.net
+// @name learn.microsoft.com
 // ==/UserScript==
 // 2024-08-18 "Improve articles appearance on `learn.microsoft.com`": https://github.com/dmitrii-fediuk/vm/issues/23
 // language=CSS
