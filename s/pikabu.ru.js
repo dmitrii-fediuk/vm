6d26d7da077403c4fde76281a90da594bd3ee396
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-21 09:00:54 +0100

    https://github.com/dmitrii-fediuk/vm/issues/30

--- s/pikabu.ru.js
+++ s/pikabu.ru.js
@@ -12,6 +12,9 @@ GM_addStyle('.main {float: none !important; padding: 0 !important; width: 100% !
 // 2024-08-21
 // language=CSS
 GM_addStyle('.sidebar {display: none !important;}');
+// 2024-05-30, 2024-08-21 "Hide the «Theme picker» sticky block from `pikabu.ru`": https://github.com/dmitrii-fediuk/vm/issues/30
+// language=CSS
+GM_addStyle('.theme-picker {display: none !important;}');
 // 2024-05-30 "Hide the left sidebar from pikabu.ru": https://github.com/dmitrii-fediuk/adblock/issues/32
 // language=CSS
 GM_addStyle('.app__inner {padding: 0 !important;}');
