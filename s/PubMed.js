2fee439e39d6144c69ca68d1c70ecf09c2f7b2f2
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-28 00:37:50 +0100

    https://github.com/dmitrii-fediuk/vm/issues/1

--- s/PubMed.js
+++ s/PubMed.js
@@ -3,7 +3,7 @@
 // @grant GM_addStyle
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/PubMed.js
 // @icon https://cdn.ncbi.nlm.nih.gov/coreutils/nwds/img/favicons/favicon-144.png
-// @match *://pubmed.ncbi.nlm.nih.gov/*
+// @match *://*.ncbi.nlm.nih.gov/*
 // @name PubMed
 // ==/UserScript==
 // 2024-06-08 "Hide the right sidebar from PubMed": https://github.com/dmitrii-fediuk/adblock/issues/48
