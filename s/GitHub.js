8f6f7753b9c7223cfef123ce109188c74c41596e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-19 22:13:51 +0100

    https://github.com/dmitrii-fediuk/vm/issues/47

---
+++ s/GitHub.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/GitHub.js
+// @icon https://github.com/fluidicon.png
+// @match *://github.com/*
+// @name GitHub
+// ==/UserScript==
+// 2024-09-19
+// "Improve the appearance of GitHub": https://github.com/dmitrii-fediuk/vm/issues/47
+// language=CSS
+GM_addStyle('#repos-sticky-header {position: initial !important;}');
