11dcd4bc694ba8d9818c311ade79723bc9bcafc9
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-27 08:01:57 +0100

    https://github.com/dmitrii-fediuk/vm/issues/49

---
+++ s/DeepL.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/DeepL.js
+// @icon https://static.deepl.com/img/favicon/tile_144.png
+// @match *://deepl.com/*
+// @name DeepL
+// ==/UserScript==
+// 2024-09-27 "Improve the appearance of DeepL": https://github.com/dmitrii-fediuk/vm/issues/49
+// language=CSS
+GM_addStyle('header {position: initial !important;}');
