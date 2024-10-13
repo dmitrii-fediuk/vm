073d3161a26bf66ddffa60c8a5cabd03c07deb7d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-13 16:58:49 +0100

    refactoring

---
+++ s/Upwork/chat-reading.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Upwork/chat-reading.js
+// @icon https://www.upwork.com/favicon.ico
+// @match *://www.upwork.com/*
+// @name Upwork
+// ==/UserScript==
+// 2024-10-13 "Remove all visual garbage from the Upwork chat screen": https://github.com/dmitrii-fediuk/vm/issues/53
+// language=CSS
---
+++ s/Upwork/default.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Upwork/default.js
+// @icon https://www.upwork.com/favicon.ico
+// @match *://www.upwork.com/*
+// @name Upwork
+// ==/UserScript==
+// 2024-10-13 "Improve the Upwork appearance": https://github.com/dmitrii-fediuk/vm/issues/52
+// language=CSS
