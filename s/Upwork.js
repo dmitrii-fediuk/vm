a4ad2362bc32d24f9153e315d510452e0e74c7a1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-13 16:55:36 +0100

    https://github.com/dmitrii-fediuk/vm/issues/53

---
+++ s/Upwork.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Upwork.js
+// @icon https://www.upwork.com/favicon.ico
+// @match *://www.upwork.com/*
+// @name Upwork
+// ==/UserScript==
+// 2024-10-13
+// 1) "Improve the Upwork appearance": https://github.com/dmitrii-fediuk/vm/issues/52
+// 2) "Remove all visual garbage from the Upwork chat screen": https://github.com/dmitrii-fediuk/vm/issues/53
+// language=CSS
