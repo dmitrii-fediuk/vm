bad2338d33b4969093f17d6a839a7145683ea60a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-16 20:01:52 +0100

    https://github.com/dmitrii-fediuk/vm/issues/55

--- s/turkishliving.com.js
+++ s/turkishliving.com.js
@@ -2,7 +2,7 @@
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @grant GM_addStyle
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/turkishliving.com.js
-// @icon https://kubernetes.io/icons/favicon-64.png
+// @icon https://turkishliving.com/forums/data/assets/logo/xTurkishLiving_PWA192.png.pagespeed.ic.AxBROYz33R.png
 // @match *://turkishliving.com/*
 // @name turkishliving.com
 // ==/UserScript==
