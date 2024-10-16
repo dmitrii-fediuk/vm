b68af0c250084485504eea8933dbee727037c501
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-16 01:33:45 +0100

    https://github.com/dmitrii-fediuk/vm/issues/54

---
+++ s/kubernetes.io.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/openvpn.net.js
+// @icon https://kubernetes.io/icons/favicon-64.png
+// @match *://kubernetes.io/*
+// @name kubernetes.io
+// ==/UserScript==
+// 2024-10-16 "Improve `kubernetes.io`": https://github.com/dmitrii-fediuk/vm/issues/54
+// language=CSS
