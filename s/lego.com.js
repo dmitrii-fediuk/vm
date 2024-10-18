ef268803dd3bdcb4e0a6c5439238eed79848993f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-18 21:55:00 +0100

    https://github.com/dmitrii-fediuk/vm/issues/56

---
+++ s/lego.com.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/lego.com.js
+// @icon https://www.lego.com/lego-logo-512.png
+// @match *://www.lego.com/*
+// @name lego.com
+// ==/UserScript==
+// 2024-10-19 "Improve `lego.com`": https://github.com/dmitrii-fediuk/vm/issues/56
+// language=CSS
