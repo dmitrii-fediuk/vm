f8e46d203b9555dd34c1794364ab8e261095c1a0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-18 21:57:04 +0100

    https://github.com/dmitrii-fediuk/vm/issues/56

--- s/lego.com.js
+++ s/lego.com.js
@@ -8,3 +8,10 @@
 // ==/UserScript==
 // 2024-10-19 "Improve `lego.com`": https://github.com/dmitrii-fediuk/vm/issues/56
 // language=CSS
+// language=CSS
+GM_addStyle([
+	'[data-test=fixed-pos-wrapper]'
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
