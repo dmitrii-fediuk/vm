e446ef6596507f4d52dae2b380a273560d8848ce
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-20 21:36:53 +0100

    https://github.com/dmitrii-fediuk/vm/issues/57

---
+++ s/Wikipedia.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Wikipedia.js
+// @icon https://www.lego.com/lego-logo-512.png
+// @match *://*.wikipedia.org/*
+// @name Wikipedia
+// ==/UserScript==
+// 2024-10-20 "Improve the appearance of Wikipedia articles": https://github.com/dmitrii-fediuk/vm/issues/57
