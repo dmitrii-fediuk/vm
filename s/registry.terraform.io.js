165dd9457b37ff21d4961292da91850b0afc428b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-21 03:56:54 +0100

    https://github.com/dmitrii-fediuk/vm/issues/58

--- s/registry.terraform.io.js
+++ s/registry.terraform.io.js
@@ -22,8 +22,9 @@ GM_addStyle([
 	'.columns-provider-docs'
 	,'.section'
 	,'article'
-	,'h1'
 ]
 	 // language=Javascript
 	.join(',') + '{display: initial !important; margin: 0 !important; padding: 0 !important; width: initial !important;}')
 ;
+// language=CSS
+GM_addStyle('h1 {margin: 0 0.5rem !important; padding: 0 !important;}');
