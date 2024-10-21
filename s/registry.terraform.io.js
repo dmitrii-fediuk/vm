0e554cc8afbdefec90756f1630fffe0daba09cea
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-21 03:26:30 +0100

    https://github.com/dmitrii-fediuk/vm/issues/58

---
+++ s/registry.terraform.io.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/registry.terraform.io.js
+// @icon https://registry.terraform.io/images/favicons/apple-touch-icon.png
+// @match *://registry.terraform.io/*
+// @name registry.terraform.io
+// ==/UserScript==
+// 2024-10-21 "Improve `registry.terraform.io`": https://github.com/dmitrii-fediuk/vm/issues/58
+// language=CSS
