74581cba678039d056335975d9c2afa34d3cbedb
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-27 16:26:51 +0100

    https://github.com/dmitrii-fediuk/vm/issues/61

--- s/Google/Maps/Street_View/common.js
+++ s/Google/Maps/Street_View/common.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @grant GM_addStyle
-// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Google/Maps/Street_View/without-controls.js
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Google/Maps/Street_View/common.js
 // @icon https://www.google.com/images/branding/product/ico/maps15_bnuw3a_64dp.ico
 // @match https://www.google.com/maps/*
-// @name Google / Maps / Street View / without controls
+// @name Google / Maps / Street View / common
 // ==/UserScript==
 // 2024-10-27 "Improve Google Maps": https://github.com/dmitrii-fediuk/vm/issues/61
 
