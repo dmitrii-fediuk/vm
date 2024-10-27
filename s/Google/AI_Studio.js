6e2f219d4ab5d165b51cbbab24c7ce5427c03c3f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-27 16:24:35 +0100

    https://github.com/dmitrii-fediuk/vm/issues/61

--- s/Google/AI_Studio.js
+++ s/Google/AI_Studio.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Google/AI_Studio.js
 // @icon https://www.gstatic.com/aistudio/ai_studio_favicon_64x64.svg
 // @match *://aistudio.google.com/*
-// @name Google // AI Studio
+// @name Google / AI Studio
 // ==/UserScript==
 // 2024-10-23 "Improve `aistudio.google.com`": https://github.com/dmitrii-fediuk/vm/issues/59
 // language=CSS
--- s/Google/Maps/Street_View/without-controls.js
+++ s/Google/Maps/Street_View/without-controls.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Google/Maps/Street_View/without-controls.js
 // @icon https://www.google.com/images/branding/product/ico/maps15_bnuw3a_64dp.ico
 // @match https://www.google.com/maps/*
-// @name Google // Maps // Street View // without controls
+// @name Google / Maps / Street View / without controls
 // ==/UserScript==
 // 2024-10-27 "Improve Google Maps": https://github.com/dmitrii-fediuk/vm/issues/61
 
