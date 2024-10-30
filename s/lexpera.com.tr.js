92addad256edf68c8e72691e3b3bd18120ae033b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-10-30 11:07:56 +0100

    https://github.com/dmitrii-fediuk/vm/issues/62

---
+++ s/lexpera.com.tr.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/lexpera.com.tr.js
+// @icon https://www.lexpera.com.tr/Content/Images/logo-lexpera-icon_launcher_192.jpg
+// @match *://www.lexpera.com.tr/*
+// @name lexpera.com.tr
+// ==/UserScript==
+// 2024-10-30 "Improve `lexpera.com.tr`": https://github.com/dmitrii-fediuk/vm/issues/62
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
