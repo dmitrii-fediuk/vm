667529b43ebe42dec9f38ccb61346e36d0076454
Dmitrii Fediuk <dfediuk@gmail.com>
2024-11-03 09:55:20 +0000

    https://github.com/dmitrii-fediuk/vm/issues/63

--- s/trendyol.com.js
+++ s/trendyol.com.js
@@ -9,7 +9,8 @@
 // 2024-11-03 "Improve `trendyol.com`": https://github.com/dmitrii-fediuk/vm/issues/63
 // language=CSS
 GM_addStyle([
-	'[data-fragment-name="TopBanner"]'
+	'.search-landings-container'
+	,'[data-fragment-name="TopBanner"]'
 ]
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
