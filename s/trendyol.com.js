dd1071d4f2d766ec416a9ef4e9eb32b3373bb7a4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-11-03 09:48:12 +0000

    https://github.com/dmitrii-fediuk/vm/issues/63

---
+++ s/trendyol.com.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/trendyol.com.js
+// @icon https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/cd/72/0e/cd720ecb-ce51-0af0-85dc-0365e75bb9ad/AppIcon-0-0-1x_U007emarketing-0-10-0-85-220.png/246x0w.webp
+// @match *://www.trendyol.com/*
+// @name Trendyol
+// ==/UserScript==
+// 2024-11-03 "Improve `trendyol.com`": https://github.com/dmitrii-fediuk/vm/issues/63
+// language=CSS
