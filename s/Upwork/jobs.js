56dfa4b3521a9a2ba72ab4fbe635e2507e066025
Dmitrii Fediuk <dfediuk@gmail.com>
2024-11-11 16:31:14 +0000

    https://github.com/dmitrii-fediuk/vm/issues/52

---
+++ s/Upwork/jobs.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Upwork/jobs.js
+// @icon https://www.upwork.com/favicon.ico
+// @match *://www.upwork.com/nx/search/jobs/*
+// @name Upwork / Chat reading
+// ==/UserScript==
+// 2024-11-11 "Improve the Upwork appearance": https://github.com/dmitrii-fediuk/vm/issues/52
