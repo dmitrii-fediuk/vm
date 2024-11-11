cd5a83a77edc0e9b052f3f2070cb926d5d0e4542
Dmitrii Fediuk <dfediuk@gmail.com>
2024-11-11 16:35:05 +0000

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/jobs.js
+++ s/Upwork/jobs.js
@@ -9,7 +9,7 @@
 // 2024-11-11 "Improve the Upwork appearance": https://github.com/dmitrii-fediuk/vm/issues/52
 // language=CSS
 GM_addStyle([
-	'.filters-list'
+	'.filters-list[data-test="FiltersList"]'
 ]
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
