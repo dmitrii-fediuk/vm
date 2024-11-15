6b76dcefdd0e894bba543e0a77104db4696dc9a3
Dmitrii Fediuk <dfediuk@gmail.com>
2024-11-15 05:54:52 +0000

    https://github.com/dmitrii-fediuk/vm/issues/65

---
+++ s/Anthropic/www.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Anthropic/docs.js
+// @icon https://mintlify.s3-us-west-1.amazonaws.com/anthropic/_generated/favicon/apple-touch-icon.png?v=3
+// @match *://www.anthropic.com/*
+// @name Anthropic / www
+// ==/UserScript==
+// 2024-11-15 "Improve `www.anthropic.com`": https://github.com/dmitrii-fediuk/vm/issues/65
+// language=CSS
+GM_addStyle([
+	'#navbar'
+]
+	 // language=Javascript
+	.join(',') + '{position: initial !important;}')
+;
