adba64607a3b587aff6944101e183a056f389bd6
Dmitrii Fediuk <dfediuk@gmail.com>
2024-11-29 23:34:17 +0000

    https://github.com/dmitrii-fediuk/vm/issues/68

---
+++ s/Claude.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Claude.js
+// @icon https://claude.ai/images/claude_app_icon.png
+// @match *://claude.ai/*
+// @name Claude
+// ==/UserScript==
+// 2024-11-30 "Improve `claude.ai`": https://github.com/dmitrii-fediuk/vm/issues/68
+// language=CSS
+GM_addStyle([
+	'.max-w-3xl'
+]
+	 // language=Javascript
+	.join(',') + '{max-width: 60rem;}')
+;
