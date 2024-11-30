1267ed2a28e2d7dcc35ad631750211429e61c192
Dmitrii Fediuk <dfediuk@gmail.com>
2024-11-30 13:00:07 +0000

    https://github.com/dmitrii-fediuk/vm/issues/69

---
+++ s/akakce.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Anthropic/akakce.js
+// @icon https://cdn.akakce.com/favicon.ico
+// @match *://*.akakce.com/*
+// @name Akakce
+// ==/UserScript==
+// 2024-11-30 "Improve `akakce.com`": https://github.com/dmitrii-fediuk/vm/issues/69
+// language=CSS
+GM_addStyle([
+	'header'
+]
+	 // language=Javascript
+	.join(',') + '{position: initial !important;}')
+;
