5f5eef6274a7ba24e1df8716f4517d4368b04a62
Dmitrii Fediuk <dfediuk@gmail.com>
2024-11-30 13:42:23 +0000

    https://github.com/dmitrii-fediuk/vm/issues/69

--- s/akakce.js
+++ s/akakce.js
@@ -9,7 +9,10 @@
 // 2024-11-30 "Improve `akakce.com`": https://github.com/dmitrii-fediuk/vm/issues/69
 // language=CSS
 GM_addStyle([
-	'#Topper'
+	// 2024-11-30
+	// «Takip et, tüm satıcılardaki indirimlerden haberdar ol.» / «Follow to stay informed about discounts from all sellers.»
+	'#ntf_w'
+	,'#Topper'
 	// 2024-11-30
 	// 1) «Ürüne puan ver» / «Rate the product»
 	// 2) «Hatalı içerik mi buldunuz? Bizimle paylaşın.» / «Did you find incorrect content? Share it with us.»
@@ -17,9 +20,9 @@ GM_addStyle([
 	,'.topBanner'
 	// 2024-11-30 «Daha düşük bir fiyat mı buldunuz? Bizimle paylaşın.» / «Did you find a lower price? Share it with us.
 	,'[id^="RCP_v"]'
+	,'footer'
 	,'section:has(a[href="/son-baktiklarim/"])' // 2024-11-30 «Son Baktıklarım» / «My Recent Views»
 	//,'[class^="pl_v"]' // 2024-11-30 «Bu Ürüne Bakanlar Bunlara Da Baktı» / «People who viewed this product also viewed these»
-	,'footer'
 ]
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
