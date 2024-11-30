fc858de8120d305736aad6c8a17fdca3126b3dd1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-11-30 14:08:12 +0000

    https://github.com/dmitrii-fediuk/vm/issues/69

--- s/akakce.js
+++ s/akakce.js
@@ -25,7 +25,7 @@ GM_addStyle([
 	// «11 satıcı içinde kargo dahil en ucuz fiyat seçeneği» / «The cheapest price option including shipping among 11 sellers»
 	,'[class^="spt_v"]'
 	// 2024-11-30 «Daha düşük bir fiyat mı buldunuz? Bizimle paylaşın.» / «Did you find a lower price? Share it with us.
-	,'[id^="pd_v"] ~ section > h2'
+	,'[id^="pd_v"] ~ section > h2' // 2024-11-30 «En Ucuz <…> Fiyatları»
 	,'[id^="RCP_v"]'
 	,'[title="Takip Listem"]'
 	,'footer'
