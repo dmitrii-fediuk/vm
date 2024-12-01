2e6b071950c49efdf4a5da09f66dc296c656bf31
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-01 12:54:03 +0000

    https://github.com/dmitrii-fediuk/vm/issues/70

---
+++ s/rossmann.com.tr.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Anthropic/rossmann.com.tr.js
+// @icon https://cdn.rossmann.com.tr/media/favicon/stores/1/favicon.png
+// @match *://www.rossmann.com.tr/*
+// @name rossmann.com.tr
+// ==/UserScript==
+// 2024-12-01 "Improve `rossmann.com.tr`": https://github.com/dmitrii-fediuk/vm/issues/70
+// language=CSS
