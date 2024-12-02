dc6e2dff5cd58ca9ac971cf5004d88cf0ba65c31
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-02 01:35:02 +0000

    https://github.com/dmitrii-fediuk/vm/issues/72

--- s/Google/Docs.js
+++ s/Google/Docs.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Google/Docs.js
 // @icon https://ssl.gstatic.com/docs/documents/images/kix-favicon-2023q4.ico
 // @match *://docs.google.com/*
-// @name Google // Docs
+// @name Google / Docs
 // ==/UserScript==
 // 2024-12-02 "Improve Google Docs": https://github.com/dmitrii-fediuk/vm/issues/72
 // language=CSS
--- s/Google/Gemini.js
+++ s/Google/Gemini.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Google/Gemini.js
 // @icon https://www.gstatic.com/lamda/images/gemini_favicon_f069958c85030456e93de685481c559f160ea06b.png
 // @match *://gemini.google.com/*
-// @name Google // Gemini
+// @name Google / Gemini
 // ==/UserScript==
 // 2024-10-23 "Improve `gemini.google.com`": https://github.com/dmitrii-fediuk/vm/issues/60
 // language=CSS
