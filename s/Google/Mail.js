c5ded351f274ffc72777c0d0d01966f9b2569dfa
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-02 19:52:17 +0000

    https://github.com/dmitrii-fediuk/vm/issues/73

--- s/Google/Mail.js
+++ s/Google/Mail.js
@@ -11,6 +11,7 @@
 GM_addStyle([
 	'[aria-label="Main menu"] ~ div:has(a[title="Gmail"])' // 2024-12-02 Logo
 	,'[data-ogsr-up]' // Google Apps, Google Account
+	,'[jscontroller]:has(> span > button[aria-label="Add reaction"])'
 	,'[role="navigation"]:has([aria-label="Meet"])' // 2024-12-02 The left sidebar with a crapware like «Chat» and «Meet»
 	// 2024-12-02 Gemini, Chat, Status, etc.
 	,'header[role="banner"] :has(> [data-tooltip="Settings"]) > :not([data-tooltip="Settings"])'
