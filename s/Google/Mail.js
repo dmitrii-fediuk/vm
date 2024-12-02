41def3c541985227ea3aab6c85c978c777888271
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-02 19:00:06 +0000

    https://github.com/dmitrii-fediuk/vm/issues/73

---
+++ s/Google/Mail.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Google/Mail.js
+// @icon https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico
+// @match *://mail.google.com/*
+// @name Google / Mail
+// ==/UserScript==
+// 2024-12-02 "Improve Gmail": https://github.com/dmitrii-fediuk/vm/issues/73
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
