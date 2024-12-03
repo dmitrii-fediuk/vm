b59f98d99618eb8524ee931db0b4de5be411095a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-03 07:07:55 +0000

    https://github.com/dmitrii-fediuk/vm/issues/51

--- s/ChatGPT.js
+++ s/ChatGPT.js
@@ -9,7 +9,7 @@
 // 2024-10-08 "Improve the appearance of ChatGPT": https://github.com/dmitrii-fediuk/vm/issues/51
 // language=CSS
 GM_addStyle([
-	'.text-base:has(form #composer-background) > .text-center' // 2024-12-03 «What can I help with?»
+	//'.text-base:has(form #composer-background) > .text-center' // 2024-12-03 «What can I help with?»
 ]
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
@@ -18,3 +18,10 @@ GM_addStyle([
 // "Hide follow-up question bubbles after ChatGPT responses": https://github.com/dmitrii-fediuk/vm/issues/50
 // language=CSS
 GM_addStyle('form[type="button"] div.absolute {display: none !important;}');
+// language=CSS
+GM_addStyle([
+	'.text-base:has(form #composer-background) > .text-center' // 2024-12-03 «What can I help with?»
+]
+	 // language=Javascript
+	.join(',') + '{background: yellow !important;}')
+;
