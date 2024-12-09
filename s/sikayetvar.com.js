ab32c5919d31240be4bd29898899fba5ce459d10
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-09 00:27:33 +0000

    https://github.com/dmitrii-fediuk/vm/issues/75

---
+++ s/sikayetvar.com.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/sikayetvar.com.js
+// @icon https://image-ikea.mncdn.com/_assets/img/ikea-app-logo.png
+// @match *://www.sikayetvar.com/*
+// @name Ikea Turkey
+// ==/UserScript==
+// 2024-12-09 "Improve `sikayetvar.com`": https://github.com/dmitrii-fediuk/vm/issues/75
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{position: initial !important;}')
+;
