03e819e79bb85e5c37621a68240fbc42724fafc7
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-13 08:51:49 +0000

    https://github.com/dmitrii-fediuk/vm/issues/76

--- s/Adobe/experienceleague.js
+++ s/Adobe/experienceleague.js
@@ -9,7 +9,8 @@
 // 2024-12-13 "Improve `experienceleague.adobe.com`": https://github.com/dmitrii-fediuk/vm/issues/76
 // language=CSS
 GM_addStyle([
-	'.doc-actions-mobile'
+	'.article-metadata-topics-wrapper'
+	,'.doc-actions-mobile'
 	,'.doc-pagination'
 	,'.docs main > .rail-right.mini-toc-container'
 	,'.feedback-ui'
