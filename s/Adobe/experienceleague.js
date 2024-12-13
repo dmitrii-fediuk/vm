4aac4a157fc2648886360734e97c2a8cfb2bd562
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-13 08:38:39 +0000

    https://github.com/dmitrii-fediuk/vm/issues/76

---
+++ s/Adobe/experienceleague.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Adobe/experienceleague.js
+// @icon https://experienceleague.adobe.com/favicon.ico
+// @match *://experienceleague.adobe.com/*
+// @name Adobe / experienceleague
+// ==/UserScript==
+// 2024-12-13 "Improve `experienceleague.adobe.com`": https://github.com/dmitrii-fediuk/vm/issues/76
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{position: initial !important;}')
+;
