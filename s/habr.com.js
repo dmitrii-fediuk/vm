92bf11e8c59d8fd705c66ea6eb976fabbf21a83e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-21 18:17:12 +0000

    https://github.com/dmitrii-fediuk/vm/issues/78

--- s/habr.com.js
+++ s/habr.com.js
@@ -15,6 +15,7 @@ GM_addStyle([
 	,'.tm-article-presenter__meta'
 	,'.tm-article-sticky-panel'
 	,'.tm-comment-navigation'
+	,'.tm-comment-thread__button'
 	,'.tm-comments-wrapper__header-aside'
 	,'.tm-feature'
 	,'.tm-footer'
