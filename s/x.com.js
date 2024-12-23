74e976313948745b4d1bd016ab49dc3fae7aa980
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-23 09:07:58 +0000

    https://github.com/dmitrii-fediuk/vm/issues/79

---
+++ s/x.com.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/x.com.js
+// @icon https://abs.twimg.com/responsive-web/client-web/icon-ios.77d25eba.png
+// @match *://x.com/*
+// @name x.com
+// ==/UserScript==
+// 2024-12-21 "Improve `x.com`": https://github.com/dmitrii-fediuk/vm/issues/79
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
