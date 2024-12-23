3e3533b175eb4e6ed41a3c504558ceac4a177524
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-23 09:10:20 +0000

    https://github.com/dmitrii-fediuk/vm/issues/79

--- s/x.com.js
+++ s/x.com.js
@@ -9,7 +9,8 @@
 // 2024-12-23 "Improve `x.com`": https://github.com/dmitrii-fediuk/vm/issues/79
 // language=CSS
 GM_addStyle([
-	'[data-testid="sidebarColumn"]'
+	'[data-testid="DMDrawer"]'
+	,'[data-testid="sidebarColumn"]'
 ]
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
