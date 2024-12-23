ec41ffa4cd0ba46353cfd1071b096770f79b8be2
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-23 10:06:34 +0000

    https://github.com/dmitrii-fediuk/vm/issues/79

--- s/x.com.js
+++ s/x.com.js
@@ -14,8 +14,9 @@ GM_addStyle([
 	,'[data-testid="UserName"] ~ div:last-of-type'
 	,'[data-testid="placementTracking"]'
 	,'[data-testid="sidebarColumn"]'
-	,'[data-testid^="UserAvatar-Container-"]'
+	//,'[data-testid^="UserAvatar-Container-"]'
 	,'div:has(> button[data-testid$="-follow"])'
+	,'div:has(> nav[aria-label="Profile timelines"]) > div:first-of-type [data-testid^="UserAvatar-Container-"]'
 	,'div:has(> nav[aria-label="Profile timelines"]) > div:first-of-type > a:has(img)'
 	,'div:has(> nav[aria-label="Profile timelines"]) > div:first-of-type > div:has(> div[style^="background-color: rgb("])'
 ]
