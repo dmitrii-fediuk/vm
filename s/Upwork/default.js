0c7a64d0897b21737800febe5a4e69ad0a10fc7e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-25 05:35:44 +0000

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/default.js
+++ s/Upwork/default.js
@@ -181,9 +181,6 @@ if (location.pathname.startsWith('/jobs/')) {
 	GM_addStyle('.qualification-items .icons {vertical-align: middle !important;}');
 	// 2024-12-25
 	// language=CSS
-	//GM_addStyle('.qualification-items > li:has(> .icons:has(.text-danger)) > * {background: red !important;}');
-	// 2024-12-25
-	// language=CSS
 	GM_addStyle('.qualification-items > li:has(.text-danger) {background: #ffebe6 !important;}');
 }
 if (location.pathname.startsWith('/nx/search/jobs')) {
