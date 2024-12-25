70c05653c8da0b78514155a162b78a8e916d812f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-25 04:12:50 +0000

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/default.js
+++ s/Upwork/default.js
@@ -117,6 +117,9 @@ if (location.pathname.startsWith('/jobs/')) {
 	GM_addStyle('.air3-card-section:has(> [data-test="Description"]) {margin-bottom: 0.66rem !important;}');
 	// 2024-12-25
 	// language=CSS
+	GM_addStyle('.air3-card-section:has(> [data-test="Segmentations"]) {margin: 0.5rem 0 !important;}');
+	// 2024-12-25
+	// language=CSS
 	GM_addStyle('[data-test="ConnectsAuction"] {display: flex !important; gap: 1rem !important;}');
 	// 2024-12-25
 	// language=CSS
