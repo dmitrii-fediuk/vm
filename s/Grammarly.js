fce695ce7832f7c8a3d192a9498e8770cad8ea28
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-29 21:46:15 +0000

    https://github.com/dmitrii-fediuk/vm/issues/81

--- s/Grammarly.js
+++ s/Grammarly.js
@@ -3,7 +3,7 @@
 // @grant GM_addStyle
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Grammarly.js
 // @icon https://denali-static.grammarly.com/files/f1d4cabc38892f860a29/apple-touch-icon-v2.png
-// @match *://app.grammarly.com/*
+// @match *://*.grammarly.com/*
 // @name Grammarly
 // ==/UserScript==
 // 2024-12-30 "Improve Grammarly": https://github.com/dmitrii-fediuk/vm/issues/81
@@ -26,3 +26,18 @@ GM_addStyle([
 	 // language=Javascript
 	.join(',') + '{margin: 0 !important;}')
 ;
+// 2024-12-30
+if (location.hostname.startsWith(('account.'))) {
+	// 2024-12-30
+	// language=CSS
+	GM_addStyle([
+		'main > [class*="description"]'
+	]
+		 // language=Javascript
+		.join(',') + '{display: none !important;}')
+	;
+}
+// 2024-12-30
+else if (location.hostname.startsWith(('app.'))) {
+
+}
