774040301ed5d8b6e06add28d1e85fa4aa4005f0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-29 21:00:38 +0000

    https://github.com/dmitrii-fediuk/vm/issues/49

--- s/DeepL.js
+++ s/DeepL.js
@@ -8,4 +8,21 @@
 // ==/UserScript==
 // 2024-09-27 "Improve the appearance of DeepL": https://github.com/dmitrii-fediuk/vm/issues/49
 // language=CSS
+GM_addStyle([
+	'#user-top-navigation-container .nav-container > [data-cy="header"]'
+	,'.air3-search'
+	,'.fe-proposal-boost-proposal'
+	,'.microapp-slot-innova'
+	,'.nav-notifications .nav-dot'
+	,'.nav-uma'
+	,'.profile-highlights'
+	,'.up-skip-nav'
+	,'[data-cy="help-menu"]'
+	,'[data-test="SaveJob"]'
+	,'footer.up-footer'
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
+// language=CSS
 GM_addStyle('header {position: initial !important;}');
