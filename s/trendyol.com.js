b5d7999f4b88ad00eb9cbef016eb0b605462ed71
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-31 03:02:30 +0000

    https://github.com/dmitrii-fediuk/vm/issues/63

--- s/trendyol.com.js
+++ s/trendyol.com.js
@@ -76,7 +76,7 @@ if ('product' === document.querySelector('meta[name="twitter:card"]')?.content) 
 	;
 	// 2024-12-31 https://chatgpt.com/c/67735dbf-bda8-800c-a506-c35441efaa30
 	// language=CSS
-	GM_addStyle('[data-testid="sliderList"] {display: grid !important; grid-template-columns: repeat(4, 1fr);}');
+	GM_addStyle('.size-variant-wrapper [data-testid="sliderList"] {display: grid !important; grid-template-columns: repeat(4, 1fr);}');
 }
 // 2024-12-31
 else if (location.pathname.startsWith(('/hesabim/siparislerim'))) {
