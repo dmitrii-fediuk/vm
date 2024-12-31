f656529f03e00721b7e0ed1b3fd9d06732e7d2f4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-12-31 23:55:14 +0000

    https://github.com/dmitrii-fediuk/vm/issues/63

--- s/trendyol.com.js
+++ s/trendyol.com.js
@@ -64,6 +64,7 @@ if ('product' === document.querySelector('meta[name="twitter:card"]')?.content) 
 		,'#header'
 		,'.ProductDetail-First-Slider' // 2024-12-31 «Sana Özel Giyim Ürünleri» / «Clothing Products Special For You»
 		,'.add-to-collections-wrapper'
+		,'.buy-now-button'
 		,'.campaigns-widget .widget-row:has([title*="Kargo Bedava"])'
 		,'.detail-attributes-title'
 		// 2025-01-01
