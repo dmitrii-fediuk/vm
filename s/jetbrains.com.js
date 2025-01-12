13aa0700fde434372ad743c882fd2fbf75792cb6
Dmitrii Fediuk <dfediuk@gmail.com>
2025-01-12 12:23:26 +0000

    https://github.com/dmitrii-fediuk/vm/issues/84

---
+++ s/jetbrains.com.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/jetbrains.com.js
+// @icon https://www.jetbrains.com/apple-touch-icon.png
+// @match *://www.jetbrains.com/*
+// @name jetbrains.com
+// ==/UserScript==
+// 2025-01-12 "Improve `jetbrains.com`": https://github.com/dmitrii-fediuk/vm/issues/84
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
