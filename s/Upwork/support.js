0def1a2d29694e377bec28ea495c895b4e9f63b6
Dmitrii Fediuk <dfediuk@gmail.com>
2025-01-24 05:12:47 +0000

    https://github.com/dmitrii-fediuk/vm/issues/86

--- s/Upwork/support.js
+++ s/Upwork/support.js
@@ -57,6 +57,8 @@ GM_addStyle('.accordion-item-content h4 {margin: 0.5rem 0 !important;}');
 // language=CSS
 GM_addStyle('.accordion-item-title {padding: 0.5rem 0 !important;}');
 // language=CSS
+GM_addStyle('.accordion-item-title h3 {font-size: 1.25rem !important;}');
+// language=CSS
 GM_addStyle([
 	'.article-page > .container'
 ]
