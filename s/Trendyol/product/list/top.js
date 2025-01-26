de18aceb6b0738fd574d990050a96db54b9207d8
Dmitrii Fediuk <dfediuk@gmail.com>
2025-01-26 04:00:05 +0000

    https://github.com/dmitrii-fediuk/vm/issues/63

---
+++ s/Trendyol/product/list/top.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Trendyol/product/list/top.js
+// @icon https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/cd/72/0e/cd720ecb-ce51-0af0-85dc-0365e75bb9ad/AppIcon-0-0-1x_U007emarketing-0-10-0-85-220.png/246x0w.webp
+// @match *://www.trendyol.com/sr/*
+// @name Trendyol / Product / List / Top
+// ==/UserScript==
+// 2024-11-03 "Improve `trendyol.com`": https://github.com/dmitrii-fediuk/vm/issues/63
+// 2025-01-26
+// language=CSS
