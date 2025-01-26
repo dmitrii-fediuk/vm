cf449d35cae9434884f1265aa8df57323a1320a0
Dmitrii Fediuk <dfediuk@gmail.com>
2025-01-26 03:55:15 +0000

    https://github.com/dmitrii-fediuk/vm/issues/63

--- s/Trendyol/product/list.js
+++ s/Trendyol/product/list.js
@@ -3,103 +3,100 @@
 // @grant GM_addStyle
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Trendyol/product/list.js
 // @icon https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/cd/72/0e/cd720ecb-ce51-0af0-85dc-0365e75bb9ad/AppIcon-0-0-1x_U007emarketing-0-10-0-85-220.png/246x0w.webp
-// @match *://www.trendyol.com/*
+// @match *://www.trendyol.com/sr/*
 // @name Trendyol / Product / List
 // ==/UserScript==
 // 2024-11-03 "Improve `trendyol.com`": https://github.com/dmitrii-fediuk/vm/issues/63
-// language=CSS
 // 2025-01-26
-if (location.pathname.startsWith(('/sr/'))) {
+// language=CSS
+GM_addStyle([
+	'.fvrt-btn-wrppr'
+	,'.image-overlay'
+	,'.price-promotion-container > :not(.prc-cntnr)'
+	,'.prvs-pg-bttn-wrppr'
+	,'.quick-look-container'
+	,'.rating-wrapper'
+	,'.social-proof-wrapper'
+	,'.variant-options-wrapper'
+	,':has(> .product-card-images-indicator)'
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
+// 2025-01-26
+// language=CSS
+GM_addStyle([
+	'.p-card-wrppr'
+]
+	 // language=Javascript
+	.join(',') + '{margin: 0 !important;}')
+;
+// 2025-01-26
+// language=CSS
+GM_addStyle([
+	'.p-card-wrppr' // 2025-01-26 https://chatgpt.com/c/67959338-9f94-800c-a07a-1d6ae0a28ec8
+	,'.search-app-container'
+	,'.srch-prdcts-cntnr'
+	,'.srch-rslt-cntnt' // 2025-01-26 https://chatgpt.com/c/67959338-9f94-800c-a07a-1d6ae0a28ec8
+]
+	 // language=Javascript
+	.join(',') + '{width: auto !important;}')
+;
+// 2025-01-26
+// language=CSS
+GM_addStyle([
+	'.price-promotion-container'
+	,'.product-down'
+]
+	 // language=Javascript
+	.join(',') + '{min-height: auto !important;}')
+;
+// 2025-01-26
+// language=CSS
+GM_addStyle([
+	'.card-border'
+	,'.p-card-img-wr'
+]
+	 // language=Javascript
+	.join(',') + '{border: 0 !important;}')
+;
+// 2025-01-26 https://chatgpt.com/c/67959338-9f94-800c-a07a-1d6ae0a28ec8
+// language=CSS
+GM_addStyle([
+	'.product-desc-sub-text'
+]
+	 // language=Javascript
+	.join(',') + '{white-space: initial !important;}')
+;
+// 2025-01-26 https://chatgpt.com/c/67959338-9f94-800c-a07a-1d6ae0a28ec8
+// language=CSS
+GM_addStyle([
+	'.p-card-wrppr'
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		Object.entries({
+			//'background': 'yellow'
+			// language=Javascript
+			//,'border': '1px solid red'
+			'flex': '0 0 calc(20% - 0.5rem)'
+			,'width': 'calc(20% - 0.5rem)'
+		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
+	'}'
+);
+// language=CSS
+//GM_addStyle('.p-card-wrppr > * {display: none !important;}');
+// 2025-01-26 https://chatgpt.com/c/679588e5-6c4c-800c-93b6-d95e92876f7b
+if ((new URL(location.href)).searchParams.has('pi')) {
 	// language=CSS
 	GM_addStyle([
-		'.fvrt-btn-wrppr'
-		,'.image-overlay'
-		,'.price-promotion-container > :not(.prc-cntnr)'
-		,'.prvs-pg-bttn-wrppr'
-		,'.quick-look-container'
-		,'.rating-wrapper'
-		,'.social-proof-wrapper'
-		,'.variant-options-wrapper'
-		,':has(> .product-card-images-indicator)'
+		'#header'
+		,'#sticky-aggregations'
+		,'.srch-prdcts-cntnr > :not(.infinite-scroll)'
+		,'[data-fragment-name="Navigation"]'
 	]
 		 // language=Javascript
 		.join(',') + '{display: none !important;}')
 	;
-	// 2025-01-26
-	// language=CSS
-	GM_addStyle([
-		'.p-card-wrppr'
-	]
-		 // language=Javascript
-		.join(',') + '{margin: 0 !important;}')
-	;
-	// 2025-01-26
-	// language=CSS
-	GM_addStyle([
-		'.p-card-wrppr' // 2025-01-26 https://chatgpt.com/c/67959338-9f94-800c-a07a-1d6ae0a28ec8
-		,'.search-app-container'
-		,'.srch-prdcts-cntnr'
-		,'.srch-rslt-cntnt' // 2025-01-26 https://chatgpt.com/c/67959338-9f94-800c-a07a-1d6ae0a28ec8
-	]
-		 // language=Javascript
-		.join(',') + '{width: auto !important;}')
-	;
-	// 2025-01-26
-	// language=CSS
-	GM_addStyle([
-		'.price-promotion-container'
-		,'.product-down'
-	]
-		 // language=Javascript
-		.join(',') + '{min-height: auto !important;}')
-	;
-	// 2025-01-26
-	// language=CSS
-	GM_addStyle([
-		'.card-border'
-		,'.p-card-img-wr'
-	]
-		 // language=Javascript
-		.join(',') + '{border: 0 !important;}')
-	;
-	// 2025-01-26 https://chatgpt.com/c/67959338-9f94-800c-a07a-1d6ae0a28ec8
-	// language=CSS
-	GM_addStyle([
-		'.product-desc-sub-text'
-	]
-		 // language=Javascript
-		.join(',') + '{white-space: initial !important;}')
-	;
-	// 2025-01-26 https://chatgpt.com/c/67959338-9f94-800c-a07a-1d6ae0a28ec8
-	// language=CSS
-	GM_addStyle([
-		'.p-card-wrppr'
-	]
-		// language=Javascript
-		.join(',') + '{' +
-			// language=CSS
-			Object.entries({
-				//'background': 'yellow'
-				// language=Javascript
-				//,'border': '1px solid red'
-				'flex': '0 0 calc(20% - 0.5rem)'
-				,'width': 'calc(20% - 0.5rem)'
-			}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
-		'}'
-	);
-	// language=CSS
-	//GM_addStyle('.p-card-wrppr > * {display: none !important;}');
-	// 2025-01-26 https://chatgpt.com/c/679588e5-6c4c-800c-93b6-d95e92876f7b
-	if ((new URL(location.href)).searchParams.has('pi')) {
-		// language=CSS
-		GM_addStyle([
-			'#header'
-			,'#sticky-aggregations'
-			,'.srch-prdcts-cntnr > :not(.infinite-scroll)'
-			,'[data-fragment-name="Navigation"]'
-		]
-			 // language=Javascript
-			.join(',') + '{display: none !important;}')
-		;
-	}
 }
