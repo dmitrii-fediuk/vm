f5da41ed99aa4e0ce772fb4c4a34b097c7c864e5
Dmitrii Fediuk <dfediuk@gmail.com>
2025-02-21 22:26:05 +0000

    https://github.com/dmitrii-fediuk/vm/issues/92

--- s/grok.com.js
+++ s/grok.com.js
@@ -96,6 +96,6 @@ GM_addStyle(':has(> form.bottom-0) {padding-bottom: 0.25rem !important;}');
 //GM_addStyle('.bottom-0 {margin: 0 auto; !important;}');
 // 2025-02-22 https://grok.com/chat/16a85b97-a902-4aba-a531-fdb131572c0c
 // language=CSS
-//GM_addStyle('.\\@container\\/main > .h-full {height: calc(100% - 96px);}');
+GM_addStyle('.\\@container\\/main > .h-full {height: auto !important;}');
 // language=CSS
 GM_addStyle('.\\@container\\/main > .bottom-0 {width: 100% !important;}');
