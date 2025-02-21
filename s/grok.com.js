6285ee6a77986014e5445cc2265396634b6deca4
Dmitrii Fediuk <dfediuk@gmail.com>
2025-02-21 22:40:50 +0000

    https://github.com/dmitrii-fediuk/vm/issues/92

--- s/grok.com.js
+++ s/grok.com.js
@@ -10,7 +10,7 @@
 // language=CSS
 GM_addStyle([
 	'.\\@container\\/main div[style^="padding-bottom"]'
-	,'.\\@container\\/nav'
+	//,'.\\@container\\/nav'
 	,'button:has(> svg.lucide-chevron-down)'
 ]
 	 // language=Javascript
