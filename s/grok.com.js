92c9863d57a697b37c6ffa3e101404e4cd485a04
Dmitrii Fediuk <dfediuk@gmail.com>
2025-02-21 20:58:34 +0000

    https://github.com/dmitrii-fediuk/vm/issues/92

--- s/grok.com.js
+++ s/grok.com.js
@@ -6,7 +6,7 @@
 // @match *://grok.com/*
 // @name grok.com
 // ==/UserScript==
-// 2025-02-11 "Improve `grok.com`": https://github.com/dmitrii-fediuk/vm/issues/92
+// 2025-02-21 "Improve `grok.com`": https://github.com/dmitrii-fediuk/vm/issues/92
 // language=CSS
 GM_addStyle([
 	'.\\@container\\/nav'
@@ -14,7 +14,7 @@ GM_addStyle([
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
 ;
-// 2025-02-11
+// 2025-02-21
 // language=CSS
 GM_addStyle([
 	//'.top-0'
@@ -22,37 +22,38 @@ GM_addStyle([
 	 // language=Javascript
 	.join(',') + '{position: initial !important;}')
 ;
-// 2025-02-11
+// 2025-02-21
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{position: absolute !important;}')
 ;
-// 2025-02-11
+// 2025-02-21
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{margin: 0 !important;}')
 ;
-// 2025-02-11
+// 2025-02-21
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{padding: 0 !important;}')
 ;
-// 2025-02-11
+// 2025-02-21
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{border: 0 !important;}')
 ;
-// 2025-02-11
+// 2025-02-21
 // language=CSS
 GM_addStyle([
+	'.sm\\:max-w-\\[90\\%\\]'
 ]
 	// language=Javascript
 	.join(',') + '{' +
