8c560032e7cc74589f70cca366c375db29ae5256
Dmitrii Fediuk <dfediuk@gmail.com>
2025-02-21 20:52:49 +0000

    https://github.com/dmitrii-fediuk/vm/issues/92

---
+++ s/grok.com.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/grokcom.js
+// @icon https://registry.npmmirror.com/@lobehub/icons-static-png/1.24.0/files/light/grok.png
+// @match *://grok.com/*
+// @name grok.com
+// ==/UserScript==
+// 2025-02-11 "Improve `grok.com`": https://github.com/dmitrii-fediuk/vm/issues/92
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
+// 2025-02-11
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{position: initial !important;}')
+;
+// 2025-02-11
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{position: absolute !important;}')
+;
+// 2025-02-11
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{margin: 0 !important;}')
+;
+// 2025-02-11
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{padding: 0 !important;}')
+;
+// 2025-02-11
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{border: 0 !important;}')
+;
+// 2025-02-11
+// language=CSS
+GM_addStyle([
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		['width', 'max-width',' min-width']
+			// language=Javascript
+			.map(k => `${k}: initial !important;`).join(' ') +
+	'}'
+);
