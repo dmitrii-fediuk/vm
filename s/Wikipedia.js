41833bf05ca5175afc2a48b683bf9f0646e7831a
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-05 02:47:26 +0000

    https://github.com/dmitrii-fediuk/vm/issues/57

--- s/Wikipedia.js
+++ s/Wikipedia.js
@@ -9,6 +9,7 @@
 // 2024-10-20 "Improve the appearance of Wikipedia articles": https://github.com/dmitrii-fediuk/vm/issues/57
 GM_addStyle([
 	'#vector-page-titlebar-toc'
+	,'.vector-sitenotice-container' // 2025-03-05
 ]
 	 // language=Javascript
 	.join(',') + '{position: initial !important;}')
