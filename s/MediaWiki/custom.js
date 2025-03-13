bfa33dda3d1e05594e7a859b87c0aed058407912
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-13 11:34:27 +0100

    https://github.com/dmitrii-fediuk/vm/issues/96

--- s/MediaWiki/custom.js
+++ s/MediaWiki/custom.js
@@ -7,56 +7,55 @@
 // @name MediaWiki / Custom
 // ==/UserScript==
 // 2025-03-13 "Improve MediaWiki-based websites": https://github.com/dmitrii-fediuk/vm/issues/96
-if (document.querySelector('meta[name="generator"]')?.content?.startsWith('MediaWiki')) {
-	// language=CSS
-	GM_addStyle([
-	]
-		 // language=Javascript
-		.join(',') + '{display: none !important;}')
-	;
-	// 2025-03-13
-	// language=CSS
-	GM_addStyle([
-	]
-		 // language=Javascript
-		.join(',') + '{position: initial !important;}')
-	;
-	// 2025-03-13
-	// language=CSS
-	GM_addStyle([
-	]
-		 // language=Javascript
-		.join(',') + '{margin: 0 !important;}')
-	;
-	// 2025-03-13
-	// language=CSS
-	GM_addStyle([
-	]
-		 // language=Javascript
-		.join(',') + '{padding: 0 !important;}')
-	;
-	// 2025-03-13
-	// language=CSS
-	GM_addStyle([
-	]
-		// language=Javascript
-		.join(',') + '{' +
-			// language=CSS
-			['width', 'max-width',' min-width']
-				// language=Javascript
-				.map(k => `${k}: initial !important;`).join(' ') +
-		'}'
-	);
-	// 2025-02-22 https://chatgpt.com/c/67b8f0dc-df0c-800c-84ed-6b8a8d71d710
-	// language=CSS
-	GM_addStyle([
-	]
-		// language=Javascript
-		.join(',') + '{' +
-			// language=CSS
-			['bottom', 'left',' right', 'top']
-				// language=Javascript
-				.map(k => `${k}: auto !important;`).join(' ') +
-		'}'
-	);
-}
+if (!document.querySelector('meta[name="generator"]')?.content?.startsWith('MediaWiki')) {return;}
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
+// 2025-03-13
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{position: initial !important;}')
+;
+// 2025-03-13
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{margin: 0 !important;}')
+;
+// 2025-03-13
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{padding: 0 !important;}')
+;
+// 2025-03-13
+// language=CSS
+GM_addStyle([
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		['width', 'max-width',' min-width']
+			// language=Javascript
+			.map(k => `${k}: initial !important;`).join(' ') +
+	'}'
+);
+// 2025-02-22 https://chatgpt.com/c/67b8f0dc-df0c-800c-84ed-6b8a8d71d710
+// language=CSS
+GM_addStyle([
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		['bottom', 'left',' right', 'top']
+			// language=Javascript
+			.map(k => `${k}: auto !important;`).join(' ') +
+	'}'
+);
