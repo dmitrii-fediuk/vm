6a6695fb2180bbb0e5467edf5b7a1992264441ee
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-15 01:23:45 +0100

    https://github.com/dmitrii-fediuk/vm/issues/97

--- s/beymen.com.js
+++ s/beymen.com.js
@@ -130,7 +130,7 @@ if ((new URL(location.href).pathname.split('/')[2])?.startsWith('p_')) {
 	// 2025-03-15
 	// language=CSS
 	GM_addStyle([
-		'.m-productDescription__infoWrapper'
+		'.m-productDescription__infoWrapper *'
 	]
 		// language=Javascript
 		.join(',') + '{' +
