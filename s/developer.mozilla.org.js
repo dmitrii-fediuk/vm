2a0b90d707582a48b7161c7d69fa4e0c4bc1cf91
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-18 19:57:21 +0100

    https://github.com/dmitrii-fediuk/vm/issues/99

--- s/developer.mozilla.org.js
+++ s/developer.mozilla.org.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/developer.mozilla.org.js
 // @icon https://developer.mozilla.org/apple-touch-icon.528534bba673c38049c2.png
 // @match *://developer.mozilla.org/*
-// @name Mozilla // Developer
+// @name Mozilla / Developer
 // ==/UserScript==
 // 2025-03-18 "Improve `developer.mozilla.org`": https://github.com/dmitrii-fediuk/vm/issues/99
 // language=CSS
