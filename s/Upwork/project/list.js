e2c9ee6ec37599027263db9cd31d43383bf3aaab
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-18 22:44:00 +0100

    https://github.com/dmitrii-fediuk/vm/issues/53

--- s/Upwork/project/list.js
+++ s/Upwork/project/list.js
@@ -205,7 +205,8 @@ GM_addStyle([
 			if (p) {
 				// 2025-03-19
 				// 1) https://stackoverflow.com/a/784547
-				// 2) https://claude.ai/chat/5d359891-1f1d-400f-bc3b-e8b30fa92867
+				// 2.1) https://claude.ai/chat/5d359891-1f1d-400f-bc3b-e8b30fa92867
+				// 2.2) https://chatgpt.com/c/67d9e839-2800-8003-9a50-5a9edd88c3a2
 				p.innerHTML = p.textContent.replace(/(?:\r\n|\r|\n)/g, '<br>');
 			}
 		};
