c79dd74885ba83dcec20a09529ad7610f50295f9
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-18 13:42:40 +0100

    https://github.com/dmitrii-fediuk/vm/issues/53

--- s/Upwork/project/list.js
+++ s/Upwork/project/list.js
@@ -85,18 +85,7 @@ GM_addStyle('#main article {padding: 0.25rem 0 !important;}');
 GM_addStyle('[data-test="proposals-tier"] {padding: 0.5rem 0 0 0 !important;}');
 // 2025-03-18
 // language=CSS
-GM_addStyle([
-	'[data-test="JobTileDetails"]'
-]
-	// language=Javascript
-	.join(',') + '{' +
-		// language=CSS
-		Object.entries({
-			'display': 'flex'
-			,'flex-direction': 'row'
-		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
-	'}'
-);
+GM_addStyle('[data-test="JobTileDetails"] {display: flex; flex-direction: row;}');
 // 2024-12-25, 2025-03-18
 // language=CSS
 //GM_addStyle('article {margin: 0.5rem 0 !important;}');
