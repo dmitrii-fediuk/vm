15157c0f6be39c012aa3fc1256f72ea201ef578d
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-18 20:01:51 +0100

    https://github.com/dmitrii-fediuk/vm/issues/99

--- s/developer.mozilla.org.js
+++ s/developer.mozilla.org.js
@@ -9,7 +9,8 @@
 // 2025-03-18 "Improve `developer.mozilla.org`": https://github.com/dmitrii-fediuk/vm/issues/99
 // language=CSS
 GM_addStyle([
-	'.sidebar-container'
+	'.article-footer'
+	,'.sidebar-container'
 	,'.sticky-header-container'
 	,'footer'
 ]
