61e2bb13ec6591941679602f3ec46cfaadf5de9d
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-18 15:55:29 +0100

    https://github.com/dmitrii-fediuk/vm/issues/53

--- s/Upwork/project/list.js
+++ s/Upwork/project/list.js
@@ -110,11 +110,12 @@ GM_addStyle([
 		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
 	'}'
 );
+// 2025-03-18 https://chatgpt.com/c/67d98719-3eec-8003-9df4-844aa046c43b
+(new MutationObserver(() => {
 // 2025-03-18
-// 1) https://chatgpt.com/c/67d953fa-c3e8-8003-858a-d60b7a270c03
-// 2) https://claude.ai/chat/a79fe12b-9d97-4a6b-87fb-304eb27c0807
-// 3) https://grok.com/chat/1f2223eb-d192-4d35-a49a-9bcc8729e1b4
-(() => {
+	// 1) https://chatgpt.com/c/67d953fa-c3e8-8003-858a-d60b7a270c03
+	// 2) https://claude.ai/chat/a79fe12b-9d97-4a6b-87fb-304eb27c0807
+	// 3) https://grok.com/chat/1f2223eb-d192-4d35-a49a-9bcc8729e1b4
 	document.querySelectorAll('#main section > article').forEach(a => {
 		const rateE = a.querySelector('li[data-test="job-type-label"] > strong');
 		if (rateE) {
@@ -125,14 +126,14 @@ GM_addStyle([
 					const lowerBound = parseInt(m[1]);
 					console.log(`The lower bound: ${lowerBound}`);
 					if (40 > lowerBound) {
+						// 2025-03-18 https://chatgpt.com/c/67d984ba-4f08-8003-a6dc-e3d70688ceab
 						a.style.display = 'none';
-						//a.remove();
 					}
 				}
 			}
 		}
 	});
-})();
+})).observe(document.querySelector('#main section'), {childList: true, subtree: true});
 (() => {
 	const stopEvent = e => {
 		e.preventDefault();
