c38187a0dc6ba01299b2c4df87fd694383dcb0fd
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-18 22:13:56 +0100

    https://github.com/dmitrii-fediuk/vm/issues/53

--- s/Upwork/project/list.js
+++ s/Upwork/project/list.js
@@ -100,7 +100,7 @@ GM_addStyle([
 		Object.entries({
 			'font-family': 'Segoie UI'
 			// language=Javascript
-			,'font-size': '140%'
+			,'font-size': '110%'
 			,'line-height': 1.2
 		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
 	'}'
