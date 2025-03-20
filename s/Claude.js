660b9dfa55f990beabe92ba6e40e63f6cfc7cf77
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-20 05:57:03 +0100

    https://github.com/dmitrii-fediuk/vm/issues/68

--- s/Claude.js
+++ s/Claude.js
@@ -23,6 +23,10 @@ GM_addStyle([
 	 // language=Javascript
 	.join(',') + '{position: initial !important;}')
 ;
+// 2025-01-04 https://chatgpt.com/c/67792143-94dc-800c-a793-809f8e8488e6
+// 2025-02-22 https://chatgpt.com/c/67b8fea1-5c7c-800c-acb7-7be0480d93dd
+// language=CSS
+GM_addStyle('.h-screen {height: auto !important; overflow: visible !important;}');
 // 2024-11-30
 // language=CSS
 GM_addStyle('.max-w-3xl {max-width: 60rem !important;}');
