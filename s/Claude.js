1ba36e069ccb9648a312d56d64710fe9165e3f78
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-20 06:14:35 +0100

    https://github.com/dmitrii-fediuk/vm/issues/68

--- s/Claude.js
+++ s/Claude.js
@@ -34,6 +34,24 @@ GM_addStyle([
 	 // language=Javascript
 	.join(',') + '{position: initial !important;}')
 ;
+// 2025-03-20
+// language=CSS
+GM_addStyle([
+	':has(> .bottom-0) > :first-child' // 2025-03-20 The content
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		['width', 'min-width']
+			// language=Javascript
+			.map(k => `${k}: initial !important;`).join(' ') +
+		// language=CSS
+		Object.entries({
+			// language=Javascript
+			'max-width': '100%'
+		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
+	'}'
+);
 // 2025-01-04 https://chatgpt.com/c/67792143-94dc-800c-a793-809f8e8488e6
 // 2025-03-20
 // Similar to:
