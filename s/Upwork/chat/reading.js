79eea6b32c21c17ba61f2a0207b14e18df640ea6
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-23 17:15:16 +0100

    https://github.com/dmitrii-fediuk/vm/issues/53

--- s/Upwork/chat/reading.js
+++ s/Upwork/chat/reading.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Upwork/chat/reading.js
 // @icon https://www.upwork.com/favicon.ico
 // @match *://www.upwork.com/*
-// @name Upwork / Chat reading
+// @name Upwork / Chat / Reading
 // ==/UserScript==
 // 2024-10-13 "Remove all visual garbage from the Upwork chat screen": https://github.com/dmitrii-fediuk/vm/issues/53
 // language=CSS
