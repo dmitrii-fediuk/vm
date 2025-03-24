8761a9f73bafd72bb907a457261e1e12596575f7
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-25 00:58:59 +0100

    https://github.com/dmitrii-fediuk/vm/issues/84

--- s/JetBrains/www.js
+++ s/JetBrains/www.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @grant GM_addStyle
-// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/jetbrains.com.js
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/JetBrains/www.js
 // @icon https://www.jetbrains.com/apple-touch-icon.png
 // @match *://www.jetbrains.com/*
-// @name jetbrains.com
+// @name JetBrains / www
 // ==/UserScript==
 // 2025-01-12 "Improve `jetbrains.com`": https://github.com/dmitrii-fediuk/vm/issues/84
 // language=CSS
