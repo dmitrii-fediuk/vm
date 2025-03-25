04eb29fdc7a682d5aaae8616cf89f73e1c4a33ba
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-25 01:29:11 +0100

    https://github.com/dmitrii-fediuk/vm/issues/84

--- s/JetBrains/intellij-support.js
+++ s/JetBrains/intellij-support.js
@@ -79,6 +79,9 @@ GM_addStyle('.article-content p {margin: 0.25rem 0 !important;}');
 // language=CSS
 GM_addStyle('.markdown {line-height: 1.4 !important;}');
 // 2025-03-25
+// language=CSS
+GM_addStyle('.vote-sum > span {line-height: 1 !important;}');
+// 2025-03-25
 document.querySelectorAll('footer.comment__footer').forEach(f => {
 	const s = f.querySelector('.vote-sum > span');
 	if (!s || !parseInt(s.textContent)) {
