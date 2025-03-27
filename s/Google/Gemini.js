af66c4a5c6fcefde6c9993d2e0059a04c1f74955
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-27 01:25:50 +0100

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini.js
+++ s/Google/Gemini.js
@@ -22,7 +22,7 @@ GM_addStyle([
 // 2025-03-27
 // language=CSS
 GM_addStyle([
-	'body' // 2025-03-27 https://chatgpt.com/c/67e48360-0cb8-8003-bcb9-394ec9b9fb47
+	//'body' // 2025-03-27 https://chatgpt.com/c/67e48360-0cb8-8003-bcb9-394ec9b9fb47
 ]
 	 // language=Javascript
 	.join(',') + '{display: block !important;}')
@@ -30,11 +30,11 @@ GM_addStyle([
 // 2025-03-27
 // language=CSS
 GM_addStyle([
-	'body' // 2025-03-27 https://chatgpt.com/c/67e48360-0cb8-8003-bcb9-394ec9b9fb47
+	//'body' // 2025-03-27 https://chatgpt.com/c/67e48360-0cb8-8003-bcb9-394ec9b9fb47
 ]
 	 // language=Javascript
 	.join(',') + '{height: initial !important;}')
 ;
 // 2025-03-27 https://chatgpt.com/c/67e48360-0cb8-8003-bcb9-394ec9b9fb47
 // language=CSS
-GM_addStyle('body {min-height: 100vh !important;}');
+//GM_addStyle('body {min-height: 100vh !important;}');
