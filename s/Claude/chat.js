c9a266b2dc952a52086010955fdb4380de4d8633
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-27 20:34:14 +0100

    https://github.com/dmitrii-fediuk/vm/issues/68

---
+++ s/Claude/chat.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Claude/chat.js
+// @icon https://claude.ai/images/claude_app_icon.png
+// @match *://claude.ai/chat/*
+// @name Claude / Chat
+// ==/UserScript==
+// 2024-11-30 "Improve `claude.ai`": https://github.com/dmitrii-fediuk/vm/issues/68
+// 2025-03-27
