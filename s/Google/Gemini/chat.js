ce3d044de8c96b3cd9985edd88613be7e9b008ae
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-28 23:31:21 +0100

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini/chat.js
+++ s/Google/Gemini/chat.js
@@ -24,7 +24,8 @@ GM_addStyle([
 // 2025-03-28
 // language=CSS
 GM_addStyle([
-	'.side-nav-menu-button'
+	'.chat-container'
+	,'.side-nav-menu-button'
 ]
 	 // language=Javascript
 	.join(',') + '{display: block !important;}')
