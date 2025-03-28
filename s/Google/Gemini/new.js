47e39a70fd1741a953c86ee1e74089c59303782b
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-28 03:56:29 +0100

    https://github.com/dmitrii-fediuk/vm/issues/60

---
+++ s/Google/Gemini/new.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Google/Gemini/new.js
+// @icon https://www.gstatic.com/lamda/images/gemini_favicon_f069958c85030456e93de685481c559f160ea06b.png
+// @match *://gemini.google.com/u/*/app
+// @name Google / Gemini / New
+// ==/UserScript==
+// 2024-10-23 "Improve `gemini.google.com`": https://github.com/dmitrii-fediuk/vm/issues/60
+// 2025-03-28
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
