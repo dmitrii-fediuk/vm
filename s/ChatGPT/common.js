bb9c0f204ee949d37968614de4251ab4b5e4d264
Dmitrii Fediuk <dfediuk@gmail.com>
2025-03-29 06:41:06 +0100

    https://github.com/dmitrii-fediuk/vm/issues/51

--- s/ChatGPT/common.js
+++ s/ChatGPT/common.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @grant GM_addStyle
-// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/ChatGPT.js
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/ChatGPT/common.js
 // @icon https://cdn.oaistatic.com/assets/apple-touch-icon-mz9nytnj.webp
 // @match *://chatgpt.com/*
-// @name ChatGPT
+// @name ChatGPT / Common
 // ==/UserScript==
 // 2024-10-08 "Improve the appearance of ChatGPT": https://github.com/dmitrii-fediuk/vm/issues/51
 // language=CSS
