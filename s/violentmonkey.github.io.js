a377113a137ae097119d78e5aa9c945c071cf3c6
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-10 11:43:13 +0100

    https://github.com/dmitrii-fediuk/vm/issues/102

--- s/violentmonkey.github.io.js
+++ s/violentmonkey.github.io.js
@@ -151,7 +151,7 @@ GM_addStyle('body {margin: 0 1rem !important;}');
 GM_addStyle('h1 {font-size: 1.5rem !important;}');
 // 2025-04-10
 // language=CSS
-GM_addStyle('h2 {margin: 0.3rem 0 !important;}');
+GM_addStyle('h2 {font-size: 1.2rem !important; margin: 0.3rem 0 !important;}');
 // 2025-04-10
 // language=CSS
 GM_addStyle('h3 {margin: 0.25rem 0 !important;}');
