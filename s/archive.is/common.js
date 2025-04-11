9440f7c1b80a728305bed182315bd930e2090eda
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-11 13:26:31 +0100

    https://github.com/dmitrii-fediuk/vm/issues/101

--- s/archive.is/common.js
+++ s/archive.is/common.js
@@ -3,9 +3,9 @@
 // @grant GM_addStyle
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/archive.is/common.js
 // @icon https://archive.is/apple-touch-icon-144x144.png
-// @include https://archive.is
-// @include https://archive.is/
-// @exclude /^https:\/\/archive\.is\/[^\/]+/
+// @match *://archive.is/
+// @match *://archive.is
+// @exclude-match *://archive.is/*/*
 // @name archive.is / Common
 // ==/UserScript==
 // 2025-04-10 "Improve `archive.is`": https://github.com/dmitrii-fediuk/vm/issues/101
