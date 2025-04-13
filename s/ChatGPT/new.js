2532c0d919b0cbfcfeffb88fa2c58d6626db0e9a
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-13 10:40:42 +0100

    https://github.com/dmitrii-fediuk/vm/issues/51

--- s/ChatGPT/new.js
+++ s/ChatGPT/new.js
@@ -32,7 +32,7 @@ GM_addStyle([
 // 2025-04-13
 // language=CSS
 GM_addStyle([
-	'.composer-parent > [style*="--vt-page-header"]' // 2025-04-13
+	'.composer-parent > [class*="--vt-page-header"]' // 2025-04-13
 ]
 	 // language=Javascript
 	.join(',') + '{position: initial !important;}')
