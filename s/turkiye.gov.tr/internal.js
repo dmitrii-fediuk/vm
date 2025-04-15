770993e6bb5f0f907156a32467cb3618d3ee30f7
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-15 11:10:31 +0100

    https://github.com/dmitrii-fediuk/vm/issues/104

--- s/turkiye.gov.tr/internal.js
+++ s/turkiye.gov.tr/internal.js
@@ -67,7 +67,7 @@ GM_addStyle([
 // 2025-04-15
 // language=CSS
 GM_addStyle([
-	,'aside#serviceHelperBlock' // 2025-04-15
+	'aside#serviceHelperBlock' // 2025-04-15
 	,'header#top' // 2025-04-15
 	,'main#serviceBlock' // 2025-04-15
 ]
