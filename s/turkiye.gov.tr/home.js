d717cfeb4dfab2c824ccbde5943c6ac2cd166b78
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-15 11:30:51 +0100

    https://github.com/dmitrii-fediuk/vm/issues/104

---
+++ s/turkiye.gov.tr/home.js
@@ -0,0 +1,160 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/turkiye.gov.tr/home.js
+// @icon https://cdn.e-devlet.gov.tr/themes/izmir/images/favicons/favicon-196x196.1.8.0.png
+// 2025-04-11 https://chatgpt.com/c/67f90733-5ca8-8003-aa11-1ed2a0e22ea1
+// @match https://www.turkiye.gov.tr/
+// @name turkiye.gov.tr / Home
+// ==/UserScript==
+// 2025-04-15 "Improve `turkiye.gov.tr`": https://github.com/dmitrii-fediuk/vm/issues/104
+// language=CSS
+GM_addStyle([
+	'#brandingBlock' // 2025-04-15
+	,'a.scroll-to-top-btn' // 2025-04-15
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{display: block !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+	'header#top' // 2025-04-15
+	,'section#pageContentBlock' // 2025-04-15
+]
+	 // language=Javascript
+	.join(',') + '{position: initial !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+	'main#serviceBlock' // 2025-04-15
+]
+	 // language=Javascript
+	.join(',') + '{margin: 0 !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+	'main#serviceBlock' // 2025-04-15
+]
+	 // language=Javascript
+	.join(',') + '{padding: 0 !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{border: 0 !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+	'#homepageHeroBlock' // 2025-04-15
+]
+	 // language=Javascript
+	.join(',') + '{background: none !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{background-color: initial !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+	'*' // 2025-04-15
+]
+	 // language=Javascript
+	.join(',') + '{box-shadow: none !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+	'*' // 2025-04-15
+]
+	 // language=Javascript
+	.join(',') + '{color: initial !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{text-align: initial !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{clear: both !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+	'#homepageHeroBlock' // 2025-04-15
+]
+	 // language=Javascript
+	.join(',') + '{height: auto !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{line-height: initial !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		['width', 'min-width']
+			// language=Javascript
+			.map(k => `${k}: initial !important;`).join(' ') +
+		// language=CSS
+		Object.entries({
+			// language=Javascript
+			'max-width': '100%'
+		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
+	'}'
+);
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{font-size: revert !important;}')
+;
+// 2025-04-15
+// language=CSS
+GM_addStyle([
+	'body'
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		Object.entries({
+			'font-family': 'Segoie UI'
+			// language=Javascript
+			,'font-size': '150%'
+			// language=CSS
+		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
+	'}'
+);
+// 2025-04-15
+// language=CSS
+GM_addStyle('body {margin: 0 1rem !important;}');
