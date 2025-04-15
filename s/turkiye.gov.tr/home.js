993c3f667b6ca2380de12860dd5f3446424c682f
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-15 12:59:52 +0100

    https://github.com/dmitrii-fediuk/vm/issues/104

--- s/turkiye.gov.tr/home.js
+++ s/turkiye.gov.tr/home.js
@@ -64,6 +64,7 @@ GM_addStyle([
 	'#homepageSearchBlock' // 2025-04-15
 	,'#homepageSearchBlock > em.first'  // 2025-04-15
 	,'#homepageServicesCard'  // 2025-04-15
+	,'#homepageShortcutsBlock' // 2025-04-15
 	,'#homepageTopServicesBlock'  // 2025-04-15
 	,'#homepageTopServicesList > li > a > em' // 2025-04-15
 	,'#homepageTopServicesList > li > a' // 2025-04-15
