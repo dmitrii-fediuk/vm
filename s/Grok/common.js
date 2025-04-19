d44d003a07d1529005b6631b0a2bab8f57adc367
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-19 12:16:51 +0100

    https://github.com/dmitrii-fediuk/vm/issues/92

--- s/Grok/common.js
+++ s/Grok/common.js
@@ -105,11 +105,15 @@ GM_addStyle([
 		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
 	'}'
 );
-// 2025-04-15
+// 2025-04-19
+// 1) `inherit` and `revert` work too.
+// 2.1) https://developer.mozilla.org/en-US/docs/Web/CSS/unset
+// 2.2) https://developer.mozilla.org/en-US/docs/Web/CSS/inherit
+// 2.3) https://developer.mozilla.org/en-US/docs/Web/CSS/revert
 // language=CSS
 GM_addStyle([
 	'.prose'
 ]
 	 // language=Javascript
-	.join(',') + '{font-size: revert !important;}')
+	.join(',') + '{font-size: unset !important;}')
 ;
