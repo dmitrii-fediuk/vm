ffa7ab0e09a98810a55601a2a970ccad39276b06
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-19 11:31:24 +0100

    refactoring

--- s/irs.gov.js
+++ s/irs.gov.js
@@ -7,12 +7,6 @@
 // @name irs.gov
 // ==/UserScript==
 // 2025-03-07 "Improve `irs.gov`": https://github.com/dmitrii-fediuk/vm/issues/94
-// language=CSS
-GM_addStyle([
-]
-	 // language=Javascript
-	.join(',') + '{display: none !important;}')
-;
 // 2025-03-07
 // language=CSS
 GM_addStyle([
@@ -21,29 +15,3 @@ GM_addStyle([
 	 // language=Javascript
 	.join(',') + '{position: initial !important;}')
 ;
-// 2025-03-07
-// language=CSS
-GM_addStyle([
-]
-	 // language=Javascript
-	.join(',') + '{margin: 0 !important;}')
-;
-// 2025-03-07
-// language=CSS
-GM_addStyle([
-]
-	 // language=Javascript
-	.join(',') + '{padding: 0 !important;}')
-;
-// 2025-03-07
-// language=CSS
-GM_addStyle([
-]
-	// language=Javascript
-	.join(',') + '{' +
-		// language=CSS
-		['width', 'max-width',' min-width']
-			// language=Javascript
-			.map(k => `${k}: initial !important;`).join(' ') +
-	'}'
-);
