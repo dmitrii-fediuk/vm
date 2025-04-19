a6bb18a3d8f7505626217c0037fe6201310be6bc
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-19 11:30:18 +0100

    refactoring

--- s/farfetch.com.js
+++ s/farfetch.com.js
@@ -7,12 +7,6 @@
 // @name Farfetch
 // ==/UserScript==
 // 2025-03-15 "Improve `farfetch.com`": https://github.com/dmitrii-fediuk/vm/issues/98
-// language=CSS
-GM_addStyle([
-]
-	 // language=Javascript
-	.join(',') + '{display: none !important;}')
-;
 // 2025-03-15
 // language=CSS
 GM_addStyle([
@@ -22,41 +16,3 @@ GM_addStyle([
 	 // language=Javascript
 	.join(',') + '{position: initial !important;}')
 ;
-// 2025-03-15
-// language=CSS
-GM_addStyle([
-]
-	 // language=Javascript
-	.join(',') + '{margin: 0 !important;}')
-;
-// 2025-03-15
-// language=CSS
-GM_addStyle([
-]
-	 // language=Javascript
-	.join(',') + '{padding: 0 !important;}')
-;
-// 2025-03-15
-// language=CSS
-GM_addStyle([
-]
-	// language=Javascript
-	.join(',') + '{' +
-		// language=CSS
-		['width', 'max-width',' min-width']
-			// language=Javascript
-			.map(k => `${k}: initial !important;`).join(' ') +
-	'}'
-);
-// 2025-02-22 https://chatgpt.com/c/67b8f0dc-df0c-800c-84ed-6b8a8d71d710
-// language=CSS
-GM_addStyle([
-]
-	// language=Javascript
-	.join(',') + '{' +
-		// language=CSS
-		['bottom', 'left',' right', 'top']
-			// language=Javascript
-			.map(k => `${k}: auto !important;`).join(' ') +
-	'}'
-);
