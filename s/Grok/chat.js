1f7f6e9dbd983d303e2d538a84e1ec489774185e
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-19 10:32:55 +0100

    https://github.com/dmitrii-fediuk/vm/issues/92

---
+++ s/Grok/chat.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Grok/chat.js
+// @icon https://registry.npmmirror.com/@lobehub/icons-static-png/1.24.0/files/light/grok.png
+// @match *://grok.com/chat/*
+// @name Grok / Chat
+// ==/UserScript==
+// 2025-02-21, 2025-04-19 "Improve `grok.com`": https://github.com/dmitrii-fediuk/vm/issues/92
