6d7b91c3c30e4736ec6b59af4c3b8615a097c7d1
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-20 08:06:13 +0100

    https://github.com/dmitrii-fediuk/vm/issues/92

--- s/Grok/share.js
+++ s/Grok/share.js
@@ -6,9 +6,10 @@
 // @match *://grok.com/share/*
 // @name Grok / Share
 // ==/UserScript==
-// 2025-02-21, 2025-04-19 "Improve `grok.com`": https://github.com/dmitrii-fediuk/vm/issues/92
+// 2025-02-21, 2025-04-20 "Improve `grok.com`": https://github.com/dmitrii-fediuk/vm/issues/92
 // language=CSS
 GM_addStyle([
+	'div[style^="padding-"]:empty' // 2025-04-20
 ]
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
