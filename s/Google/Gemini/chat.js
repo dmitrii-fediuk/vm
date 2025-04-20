980f830f044ad70d13788ce222ec5b6a372368e5
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-20 15:56:24 +0100

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini/chat.js
+++ s/Google/Gemini/chat.js
@@ -119,6 +119,9 @@ GM_addStyle([
 		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
 	'}'
 );
+// 2025-03-28 https://chatgpt.com/c/67e63433-b778-8003-9129-50e335c342b0
+// language=CSS
+GM_addStyle('infinite-scroller {overflow-y: auto !important;}');
 // 2025-03-28
 // language=CSS
 GM_addStyle([
@@ -156,12 +159,6 @@ GM_addStyle([
 // 2025-03-28
 // language=CSS
 GM_addStyle('body {padding: 0 0 0 1rem !important; width: auto !important;}');
-// 2025-03-29
-// language=CSS
-//GM_addStyle('.side-nav-menu-button {margin: 0 0 0.5rem 0 !important;}');
-// 2025-03-28 https://chatgpt.com/c/67e63433-b778-8003-9129-50e335c342b0
-// language=CSS
-GM_addStyle('infinite-scroller {overflow-y: auto !important;}');
 // 2025-03-28
 // language=CSS
 GM_addStyle([
