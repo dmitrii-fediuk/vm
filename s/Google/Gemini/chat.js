77fc403f93431218356462962ef32697d85006fc
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-20 16:22:10 +0100

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini/chat.js
+++ s/Google/Gemini/chat.js
@@ -129,7 +129,7 @@ GM_addStyle([
 ;
 // 2025-03-28 https://chatgpt.com/c/67e63433-b778-8003-9129-50e335c342b0
 // language=CSS
-GM_addStyle('infinite-scroller {overflow-y: auto !important;}');
+//GM_addStyle('infinite-scroller {overflow-y: auto !important;}');
 // 2025-03-28
 // language=CSS
 GM_addStyle([
