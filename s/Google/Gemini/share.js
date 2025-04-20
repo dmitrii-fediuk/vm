e88305eb9f52d466fa9a612026df8dcde0c68d7c
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-20 16:28:17 +0100

    https://github.com/dmitrii-fediuk/vm/issues/60

---
+++ s/Google/Gemini/share.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Google/Gemini/share.js
+// @icon https://registry.npmmirror.com/@lobehub/icons-static-png/1.42.0/files/dark/gemini-color.png
+// @match *://gemini.google.com/share/*
+// @name Google / Gemini / Share
+// ==/UserScript==
+// 2025-04-20 "Improve `gemini.google.com`": https://github.com/dmitrii-fediuk/vm/issues/60
+// 2025-04-20
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
