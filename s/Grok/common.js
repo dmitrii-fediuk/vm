91895da627734c6f3e0fb67e82da7f9a6786866c
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-20 08:18:01 +0100

    https://github.com/dmitrii-fediuk/vm/issues/92

--- s/Grok/common.js
+++ s/Grok/common.js
@@ -204,7 +204,7 @@ GM_addStyle([
 );
 // 2025-02-22
 // language=CSS
-GM_addStyle('.\\@container\\/main > div {padding: 0.25rem !important;}');
+//GM_addStyle('.\\@container\\/main > div {padding: 0.25rem !important;}');
 // 2025-02-22
 // language=CSS
 GM_addStyle('.message-bubble {padding: 0.5rem !important;}');
