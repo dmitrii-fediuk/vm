46ce74984255cbdd622e4f00a32cec3705507d40
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-20 12:35:30 +0100

    https://github.com/dmitrii-fediuk/vm/issues/47

---
+++ s/GitHub/wiki-history.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/GitHub/wiki-history.js
+// @icon https://github.com/fluidicon.png
+// @match *://github.com/*/*/wiki/*/_history
+// @name GitHub / Wiki / History
+// ==/UserScript==
+// 2024-09-20, 2025-04-18 "Improve the GitHub appearance": https://github.com/dmitrii-fediuk/vm/issues/47
+console.log('History');
