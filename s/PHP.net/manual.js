d9c87bf4fa61c6f75a17cfb33377c264df3796d9
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-25 18:09:21 +0100

    https://github.com/dmitrii-fediuk/vm/issues/107

--- s/PHP.net/manual.js
+++ s/PHP.net/manual.js
@@ -146,7 +146,7 @@ GM_addStyle([
 	'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
 ]
 	 // language=Javascript
-	.join(',') + '{font-weight: revert !important;}')
+	.join(',') + '{all: revert !important;}')
 ;
 // 2025-04-25
 // language=CSS
