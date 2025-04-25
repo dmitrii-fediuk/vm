3e1d530908791dc26b5360f8b90fefbb55448064
Dmitrii Fediuk <dfediuk@gmail.com>
2025-04-25 17:13:49 +0100

    https://github.com/dmitrii-fediuk/vm/issues/107

--- s/PHP.net/manual.js
+++ s/PHP.net/manual.js
@@ -9,7 +9,8 @@
 // 2025-04-25 "Improve ``php.net`": https://github.com/dmitrii-fediuk/vm/issues/107
 // language=CSS
 GM_addStyle([
-	'aside.layout-menu'
+	'.change-language'
+	,'aside.layout-menu'
 	,'nav.navbar-fixed-top'
 ]
 	 // language=Javascript
