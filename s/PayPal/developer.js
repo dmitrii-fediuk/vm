3759bc532f736442fd4f77a393189ecb6c88b50c
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-04 20:51:52 +0100

    https://github.com/dmitrii-fediuk/vm/issues/109

--- s/PayPal/developer.js
+++ s/PayPal/developer.js
@@ -17,7 +17,7 @@ GM_addStyle([
 // 2025-05-04
 // language=CSS
 GM_addStyle([
-	'.feedbackButton'
+	'[class*="feedbackButton"]'
 ]
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
