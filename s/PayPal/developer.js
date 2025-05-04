9ff6e3eeae429baf5bc899bb024df63b9989ad5f
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-04 23:46:21 +0100

    https://github.com/dmitrii-fediuk/vm/issues/109

--- s/PayPal/developer.js
+++ s/PayPal/developer.js
@@ -228,14 +228,9 @@ GM_addStyle([
 	// language=Javascript
 	.join(',') + '{' +
 		// language=CSS
-		['width', 'min-width']
+		['max-width', 'width', 'min-width']
 			// language=Javascript
-			.map(k => `${k}: initial !important;`).join(' ') +
-		// language=CSS
-		Object.entries({
-			// language=Javascript
-			'max-width': '100%'
-		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
+			.map(k => `${k}: 100% !important;`).join(' ') +
 	'}'
 );
 // 2025-05-04
