3e982c589f2a365c11370c87b76d9cdee274caff
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-04 19:50:06 +0100

    https://github.com/dmitrii-fediuk/vm/issues/109

--- s/PayPal/developer.js
+++ s/PayPal/developer.js
@@ -108,7 +108,7 @@ GM_addStyle([
 // 2025-05-04
 // language=CSS
 GM_addStyle([
-	'body :is(*)'
+	'body :is(*) > :is(*)'
 ]
 	 // language=Javascript
 	.join(',') + '{font-family: unset !important;}')
