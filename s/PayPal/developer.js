0f1f81e82286c255035f2ec16f8b9cdccabe5403
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-04 21:50:30 +0100

    https://github.com/dmitrii-fediuk/vm/issues/109

--- s/PayPal/developer.js
+++ s/PayPal/developer.js
@@ -52,7 +52,7 @@ GM_addStyle([
 	,'body > *'
 	// 2025-05-04
 	// 1.1) https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_cascade/Specificity#the_is_not_has_and_css_nesting_exceptions
-	// 1.2)
+	// 1.2) https://archive.is/5HYgv#selection-10675.63-10675.172
 	,'main:not(#A)'
 ]
 	 // language=Javascript
