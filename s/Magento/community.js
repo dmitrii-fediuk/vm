8c795727c0c89c3a8bbf21eff13c9620bfe82fad
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-05 03:10:16 +0100

    https://github.com/dmitrii-fediuk/vm/issues/110

--- s/Magento/community.js
+++ s/Magento/community.js
@@ -318,7 +318,7 @@ GM_addStyle([
 			// language=Javascript
 			,'font-size': '185%'
 			// language=CSS
-			,'line-height': 1
+			,'line-height': 1.1
 		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
 	'}'
 );
