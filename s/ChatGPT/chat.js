c7b1be5c7e8ea202997ab52c57f9d781b5bed77d
Dmitrii Fediuk <dfediuk@gmail.com>
2025-06-19 19:13:46 +0100

    https://github.com/dmitrii-fediuk/vm/issues/51

--- s/ChatGPT/chat.js
+++ s/ChatGPT/chat.js
@@ -66,6 +66,14 @@ GM_addStyle([
 // 2025-06-19
 // language=CSS
 GM_addStyle([
+	'td' // 2025-06-19
+]
+	 // language=Javascript
+	.join(',') + '{min-width: unset !important;}')
+;
+// 2025-06-19
+// language=CSS
+GM_addStyle([
 	'[class*="tableContainer"]' // 2025-06-19
 ]
 	 // language=Javascript
