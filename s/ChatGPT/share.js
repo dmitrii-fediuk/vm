0a6556eaca804cff39f76ba2bfd5f92a16a50105
Dmitrii Fediuk <dfediuk@gmail.com>
2025-06-21 17:41:41 +0100

    https://github.com/dmitrii-fediuk/vm/issues/51

--- s/ChatGPT/share.js
+++ s/ChatGPT/share.js
@@ -24,7 +24,7 @@ GM_addStyle([
 // 2025-06-21
 // language=CSS
 GM_addStyle([
-	'.border-b' // 2025-06-21
+	'.border-b:empty' // 2025-06-21
 	,':has(> a[href$="/report-content/"])' // 2025-06-21
 	,':has(> button[aria-label="Try ChatGPT"])' // 2025-06-21
 	,'a[href$="/?utm_src=deep-research-sharing"]' // 2025-06-21
