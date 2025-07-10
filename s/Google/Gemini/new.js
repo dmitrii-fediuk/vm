6693a3ea8a4719e40b07f8ed49166f7328095c88
Dmitrii Fediuk <dfediuk@gmail.com>
2025-07-10 06:50:24 +0100

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini/new.js
+++ s/Google/Gemini/new.js
@@ -3,7 +3,7 @@
 // @grant GM_addStyle
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Google/Gemini/new.js
 // @icon https://registry.npmmirror.com/@lobehub/icons-static-png/1.42.0/files/dark/gemini-color.png
-// @match *://gemini.google.com/u/1/app
+// @match *://gemini.google.com/u/*/app
 // @name Google / Gemini / New
 // ==/UserScript==
 // 2024-10-23 "Improve `gemini.google.com`": https://github.com/dmitrii-fediuk/vm/issues/60
