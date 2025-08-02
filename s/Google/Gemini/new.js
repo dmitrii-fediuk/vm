a2d2662045bed58d1b933f3caad97ad32c316a6a
Dmitrii Fediuk <dfediuk@gmail.com>
2025-08-02 15:08:02 +0100

    ⠀

--- s/Google/Gemini/new.js
+++ s/Google/Gemini/new.js
@@ -8,16 +8,19 @@
 // @name Google / Gemini / New
 // ==/UserScript==
 // 2024-10-23 "Improve `gemini.google.com`": https://github.com/dmitrii-fediuk/vm/issues/60
-// 2025-03-28
-// language=CSS
-GM_addStyle([
-	//'chat-window-content' // 2025-08-02
-	'.aurora-greeting-container' // 2025-08-02
-	,'.parent-condensed-prompt-card-container' // 2025-08-02
-]
-	 // language=Javascript
-	.join(',') + '{display: none !important;}'
-);
+// 2025-08-02
+setTimeout(() => {
+	// 2025-03-28
+	// language=CSS
+	GM_addStyle([
+		//'chat-window-content' // 2025-08-02
+		'.aurora-greeting-container' // 2025-08-02
+		,'.parent-condensed-prompt-card-container' // 2025-08-02
+	]
+		 // language=Javascript
+		.join(',') + '{display: none !important;}'
+	);
+}, 1000);
 // 2025-08-02
 // language=CSS
 GM_addStyle([
