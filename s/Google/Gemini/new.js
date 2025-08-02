75d92660132e67de35b4d5e93b560d8493489cae
Dmitrii Fediuk <dfediuk@gmail.com>
2025-08-02 11:39:50 +0100

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini/new.js
+++ s/Google/Gemini/new.js
@@ -8,8 +8,8 @@
 // @name Google / Gemini / New
 // ==/UserScript==
 // 2024-10-23 "Improve `gemini.google.com`": https://github.com/dmitrii-fediuk/vm/issues/60
-// 2025-06-15
-const p = () => {
+// 2025-08-02
+setTimeout(() => {
 	// 2025-03-28
 	// language=CSS
 	GM_addStyle([
@@ -20,53 +20,4 @@ const p = () => {
 		 // language=Javascript
 		.join(',') + '{display: none !important;}')
 	;
-};
-// 2025-06-15
-const waitForAddition = s => {
-	return new Promise(resolve => {
-		if (document.querySelector(s)) {
-			resolve();
-		}
-		else {
-			const observer = new MutationObserver(mm => {
-				mm.forEach(m => {
-					m.addedNodes.forEach(n => {
-						if (Node.ELEMENT_NODE === n.nodeType && n.matches(s)) {
-							observer.disconnect();
-							resolve();
-						}
-					});
-				});
-			});
-			observer.observe(document.body, {childList: true, subtree: true});
-		}
-	});
-};
-// 2025-06-15
-const waitForRemoval = s => {
-	return new Promise(resolve => {
-		if (!document.querySelector(s)) {
-			resolve();
-		}
-		else {
-			const observer = new MutationObserver(mm => {
-				mm.forEach(m => {
-					m.removedNodes.forEach(n => {
-						if (n.matches && n.matches(s)) {
-							observer.disconnect();
-							resolve();
-						}
-					});
-				});
-			});
-			observer.observe(document.body, {childList: true, subtree: true});
-		}
-	});
-};
-// 2025-06-15
-const t = 'mat-progress-bar';
-waitForAddition(t).then(() => {
-	waitForRemoval(t).then(() => {
-		setTimeout(p, 200);
-	})
-});
+}, 200);
