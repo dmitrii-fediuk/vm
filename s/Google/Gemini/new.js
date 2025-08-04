b3e3ecc59ad3892065dbe5febb72d90d4c2f3e83
Dmitrii Fediuk <dfediuk@gmail.com>
2025-08-04 13:49:31 +0100

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini/new.js
+++ s/Google/Gemini/new.js
@@ -6,6 +6,10 @@
 // @match *://gemini.google.com/app
 // @match *://gemini.google.com/u/*/app
 // @name Google / Gemini / New
+// 2025-08-03
+// 1) https://g.co/gemini/share/a3117147d580
+// 2) https://g.co/gemini/share/aeadd7b5e03f
+// @run-at document-start
 // ==/UserScript==
 // 2024-10-23 "Improve `gemini.google.com`": https://github.com/dmitrii-fediuk/vm/issues/60
 // 2025-03-28
