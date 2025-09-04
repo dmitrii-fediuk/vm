a8a178b132c8b0699e937cef34c3952d3bfb1fd6
Dmitrii Fediuk <dfediuk@gmail.com>
2025-09-04 18:31:21 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/item.js
+++ s/Upwork/project/item.js
@@ -6,7 +6,7 @@
 // @match *://www.upwork.com/jobs/*
 // @name Upwork / Project / Item
 // ==/UserScript==
-// 2024-10-13 'Improve the Upwork appearance': https://github.com/dmitrii-fediuk/vm/issues/52
+// 2024-10-13 "Improve the Upwork appearance": https://github.com/dmitrii-fediuk/vm/issues/52
 // 2024-12-24 A job post, e.g.:
 // «https://www.upwork.com/jobs/Developer-for-Backend-Development-with-high-Stripe-API-Experience_~021871538070777433492»
 // 2025-09-04 The previous value: «AboutClientUser»
@@ -22,7 +22,7 @@ setTimeout(() => {
 		,`.extra-jobs-cards > [class*='card']:has([data-test='SimilarJobs'])`
 		,`.nav-parent-wrapper`
 		,`.popover` // 2025-09-04
-		,`[data-test='${aboutClient}'] div:has([data-testid='head-title'])` // 2025-09-04
+		,`[data-test='${aboutClient}'] div:has([data-testid="head-title"])` // 2025-09-04
 		// 2024-12-25 The title: «Attachment»
 		,`[data-test='Attachments'] h5`
 		// 2024-12-25 The title: «Activity on this job»
@@ -52,7 +52,7 @@ setTimeout(() => {
 }, 200);
 // language=CSS
 GM_addStyle([
-	'[data-test='Features'] > ul.features > li'
+	'[data-test="Features"] > ul.features > li'
 ]
 	 // language=Javascript
 	.join(',') + '{display: initial !important;}')
@@ -68,9 +68,9 @@ GM_addStyle([
 GM_addStyle([
 	'.air3-card-section h4'
 	,'.job-details-content footer'
-	,'[data-test='ConnectsAuction']'
-	,'[data-test='Expertise'] .skills-list'
-	,'[data-test='Questions'] li'
+	,'[data-test="ConnectsAuction"]'
+	,'[data-test="Expertise"] .skills-list'
+	,'[data-test="Questions"] li'
 	,`[data-test='${aboutClient}'] div:has(> [data-test='UpCPopover'])`
 	,`[data-test='${aboutClient}'] ul.features`
 ]
@@ -106,13 +106,13 @@ GM_addStyle(`[data-test='${aboutClient}'] {order: 1 !important;}`);
 GM_addStyle('.air3-card-section:has(> h4) {order: 2 !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('.air3-card-section:has(> [data-test='Description']) {order: 3 !important;}');
+GM_addStyle('.air3-card-section:has(> [data-test="Description"]) {order: 3 !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('[data-test='Questions'] {order: 4 !important;}');
+GM_addStyle('[data-test="Questions"] {order: 4 !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('.air3-card-section:has(> [data-test='Qualifications']) {order: 5 !important;}');
+GM_addStyle('.air3-card-section:has(> [data-test="Qualifications"]) {order: 5 !important;}');
 // language=CSS
 GM_addStyle('.air3-card-section {border: 0 !important; padding: 0 0.5rem !important;}');
 // 2024-12-25
@@ -120,23 +120,23 @@ GM_addStyle('.air3-card-section {border: 0 !important; padding: 0 0.5rem !import
 GM_addStyle('.air3-card-section:first-of-type > div {margin-top: 0 !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('.air3-card-section:has(> [data-test='Description']) {margin-bottom: 0.66rem !important;}');
+GM_addStyle('.air3-card-section:has(> [data-test="Description"]) {margin-bottom: 0.66rem !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('.air3-card-section:has(> [data-test='Segmentations']) {margin: 0.5rem 0 !important;}');
+GM_addStyle('.air3-card-section:has(> [data-test="Segmentations"]) {margin: 0.5rem 0 !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('[data-test='ConnectsAuction'] {display: flex !important; gap: 1rem !important;}');
+GM_addStyle('[data-test="ConnectsAuction"] {display: flex !important; gap: 1rem !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('[data-test='ConnectsAuction'] > div {margin-top: 0 !important;}');
+GM_addStyle('[data-test="ConnectsAuction"] > div {margin-top: 0 !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('[data-test='Description'] br {display: block !important; content: ''; height: 1px !important;}');
+GM_addStyle('[data-test="Description"] br {display: block !important; content: ""; height: 1px !important;}');
 // language=CSS
 GM_addStyle([
-	'[data-test='Description'] p'
-	,'[data-test='Questions'] ol'
+	'[data-test="Description"] p'
+	,'[data-test="Questions"] ol'
 ]
 	// language=Javascript
 	.join(',') + '{' +
@@ -161,10 +161,10 @@ GM_addStyle([
 // 2024-12-25
 // language=CSS
 GM_addStyle([
-	'.air3-card-section:has(> [data-test='ConnectsAuction'])'
-	,'.air3-card-section:has(> [data-test='Qualifications'])'
-	,'[data-test='Expertise'] > [class*='grid-container']'
-	,'[data-test='Features'] .features'
+	'.air3-card-section:has(> [data-test="ConnectsAuction"])'
+	,'.air3-card-section:has(> [data-test="Qualifications"])'
+	,'[data-test="Expertise"] > [class*="grid-container"]'
+	,'[data-test="Features"] .features'
 	,`[data-test='${aboutClient}'] ul.features`
 	,`[data-test='${aboutClient}']`
 ]
@@ -173,10 +173,10 @@ GM_addStyle([
 ;
 // 2024-12-25
 // language=CSS
-GM_addStyle('[data-test='AboutClientUser'] ul.features {flex-direction: row !important;}');
+GM_addStyle('[data-test="AboutClientUser"] ul.features {flex-direction: row !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('[data-test='Questions'] {font-weight: bold; margin-bottom: 0.5rem !important;}');
+GM_addStyle('[data-test="Questions"] {font-weight: bold; margin-bottom: 0.5rem !important;}');
 // 2024-12-25
 // language=CSS
 GM_addStyle('.qualification-items .icons {vertical-align: middle !important;}');
@@ -185,16 +185,16 @@ GM_addStyle('.qualification-items .icons {vertical-align: middle !important;}');
 GM_addStyle('.qualification-items > li:has(.text-danger) {background: #ffebe6 !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle(':has(> [data-test='BudgetAmount']) {background: #ffebe6 !important;}');
+GM_addStyle(':has(> [data-test="BudgetAmount"]) {background: #ffebe6 !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('[data-test='Features'] {margin: 0.5rem 0 0 0 !important;}');
+GM_addStyle('[data-test="Features"] {margin: 0.5rem 0 0 0 !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('[data-test='Bids'] h5 {font-size: 100% !important;}');
+GM_addStyle('[data-test="Bids"] h5 {font-size: 100% !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('.extra-jobs-cards[class*='px-'] {padding: 0 0.5rem !important;}');
+GM_addStyle('.extra-jobs-cards[class*="px-"] {padding: 0 0.5rem !important;}');
 // 2024-12-25
 // language=CSS
-GM_addStyle('[data-test='WorkHistory'][class*='mt-'] {border: 0 !important; margin-top: 0.5rem !important;}');
+GM_addStyle('[data-test="WorkHistory"][class*="mt-"] {border: 0 !important; margin-top: 0.5rem !important;}');
