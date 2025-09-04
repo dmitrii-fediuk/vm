f6d8402e3e5f4539e584e9629632114294364f29
Dmitrii Fediuk <dfediuk@gmail.com>
2025-09-04 18:31:35 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/item.js
+++ s/Upwork/project/item.js
@@ -22,7 +22,7 @@ setTimeout(() => {
 		,`.extra-jobs-cards > [class*='card']:has([data-test='SimilarJobs'])`
 		,`.nav-parent-wrapper`
 		,`.popover` // 2025-09-04
-		,`[data-test='${aboutClient}'] div:has([data-testid="head-title"])` // 2025-09-04
+		,`[data-test='${aboutClient}'] div:has([data-testid='head-title'])` // 2025-09-04
 		// 2024-12-25 The title: «Attachment»
 		,`[data-test='Attachments'] h5`
 		// 2024-12-25 The title: «Activity on this job»
