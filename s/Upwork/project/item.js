34680b0ca87b8739b3951d7dbad6ca7aaa6b16a2
Dmitrii Fediuk <dfediuk@gmail.com>
2025-09-05 04:14:18 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/item.js
+++ s/Upwork/project/item.js
@@ -58,6 +58,7 @@ setTimeout(() => {
 		,`section :has(> .qualification-items:empty)` // 2025-09-05
 		,`section.air3-card-section > h5` // 2025-09-04
 		,`section.air3-card-section > section > h5` // 2025-09-04
+		,`section:has(> .client-activity-items) ~ section` // 2025-09-05
 	]
 		 // language=Javascript
 		.join(',') + '{display: none !important;}')
