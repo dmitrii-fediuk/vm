22408f30e81cd92ac4760b7b02da0ab8f1f75bf2
Dmitrii Fediuk <dfediuk@gmail.com>
2025-09-05 03:43:17 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/item.js
+++ s/Upwork/project/item.js
@@ -117,6 +117,14 @@ GM_addStyle([
 ;
 // 2025-09-05
 // language=CSS
+GM_addStyle([
+	`.posted-on-line` // 2025-09-05
+]
+	 // language=Javascript
+	.join(`,`) + `{font-weight: bold !important;}`)
+;
+// 2025-09-05
+// language=CSS
 GM_addStyle('body {margin: 0.5rem 0.5rem 0 0.5rem !important;}');
 // 2024-12-24 https://chatgpt.com/c/676b1c1b-79a0-800c-a2ca-88de2346828f
 // language=CSS
