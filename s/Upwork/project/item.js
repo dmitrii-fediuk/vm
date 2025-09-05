effe6ac8d79e3d66319981b6022d71c4193bc8e0
Dmitrii Fediuk <dfediuk@gmail.com>
2025-09-05 01:55:39 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/item.js
+++ s/Upwork/project/item.js
@@ -32,7 +32,6 @@ setTimeout(() => {
 		// 1.2) https://archive.is/5HYgv#selection-10675.63-10675.172
 		// 2025-09-04
 		,`.posted-on-line ~ :not(#A)`
-		,`.posted-on-line:not(#A)` // 2025-09-05
 		,`:has(> .skills-list) > strong` // 2025-09-04
 		,`[data-test='${aboutClient}'] div:has([data-testid='head-title'])` // 2025-09-04
 		// 2024-12-25 The title: «Attachment»
@@ -70,13 +69,6 @@ GM_addStyle([
 ]
 	 // language=Javascript
 	.join(`,`) + `{display: initial !important;}`)
-;
-// language=CSS
-GM_addStyle([
-	`.posted-on-line span` // 2025-09-05
-]
-	 // language=Javascript
-	.join(`,`) + `{display: block !important;}`)
 ;
 // language=CSS
 GM_addStyle([
