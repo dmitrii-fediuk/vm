99087e9861bd673443ad01d719d4983d6b82448d
Dmitrii Fediuk <dfediuk@gmail.com>
2025-09-06 08:05:32 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/item.js
+++ s/Upwork/project/item.js
@@ -220,7 +220,7 @@ GM_addStyle(`[data-test='Description'] br {
 	content: ''; 
 	display: block !important;
 	font-size: 0 !important;
-	height: 1px !important;
+	height: 0 !important;
 	margin: 0.5em 0 !important;
 }`);
 // 2025-09-06
