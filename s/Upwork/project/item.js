a63be65e6000d9eb20f54ca1c5197f8e50de6b87
Dmitrii Fediuk <dfediuk@gmail.com>
2025-09-06 03:17:15 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/item.js
+++ s/Upwork/project/item.js
@@ -26,22 +26,18 @@ setTimeout(() => {
 		`.${dfQuestions} > p`
 		,`.client-activity-items .help-icon`
 		,`.extra-jobs-cards > [class*='card']:has([data-test='SimilarJobs'])`
-		,`.job-details-content > :has(> button polygon)` // 2025-09-04
-		,`.nav-parent-wrapper`
-		,`.popover` // 2025-09-04
-		// 2025-05-04
-		// 1.1) https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_cascade/Specificity#the_is_not_has_and_css_nesting_exceptions
-		// 1.2) https://archive.is/5HYgv#selection-10675.63-10675.172
-		// 2025-09-04
-		,`.posted-on-line ~ :not(#A)`
-		,`:has(> .skills-list) > strong` // 2025-09-04
-		// 2024-12-25 Titles like: «Full Stack Development Deliverables»
-		,`[data-test='Expertise'] > [class*='grid-container'] > div:has(> .skills-list) > strong`
-		,'.features > li:has(#featured-job)' // 2025-09-04
 		,`.features .air3-icon` // 2025-09-04
 		,`.features .description` // 2025-09-04
 		,`.features > li::after` // 2025-09-04
+		,`.features > li:has(#featured-job)` // 2025-09-04
+		,`.job-details-content > :has(> button polygon)` // 2025-09-04
+		,`.nav-parent-wrapper`
+		,`.popover` // 2025-09-04
+		,`.posted-on-line ~ :not(#A)` // 2025-09-04
 		,`.segmentations > li > :first-child` // 2024-12-25, 2025-09-04 The title: «Project Type:»
+		,`:has(> .skills-list) > strong` // 2025-09-04
+		// 2024-12-25 Titles like: «Full Stack Development Deliverables»
+		,`[data-test='Expertise'] > [class*='grid-container'] > div:has(> .skills-list) > strong`
 		,`[data-test='${aboutClient}'] div:has([data-testid='head-title'])` // 2025-09-04
 		,`[data-test='ClientActivity'] ~ section:not([data-test='Bids'])` // 2025-09-05
 		,`[data-test='Description'] > strong:first-child` // 2025-09-04
