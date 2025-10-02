66f4ad34ebdc9d738349e4a080321c1be8f752b8
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-02 07:56:05 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list.js
+++ s/Upwork/project/list.js
@@ -161,6 +161,23 @@ GM_addStyle([
 			};
 		})();
 		/**
+		 * 2025-10-02
+		 * @return {boolean}
+		 */
+		const fRateNotSpecified = (() => {
+			const enable = new URL(location.href).searchParams.has('df-rate-not-specified');
+			return a => {
+				let r = true;
+				if (enable) {
+					const terms = a.querySelector('li[data-test="job-type-label"] > strong');
+					if (terms) {
+						r = 'Hourly' === terms.textContent.trim();
+					}
+				}
+				return r;
+			};
+		})();
+		/**
 		 * 2025-03-18
 		 * @return {boolean}
 		 */
@@ -436,7 +453,7 @@ GM_addStyle([
 		// 2) https://claude.ai/chat/a79fe12b-9d97-4a6b-87fb-304eb27c0807
 		// 3) https://grok.com/chat/1f2223eb-d192-4d35-a49a-9bcc8729e1b4
 		return c => c.querySelectorAll('article').forEach(a => {
-			if (!fRate(a) || !fPhrases(a)) {
+			if (!fRate(a) || !fRateNotSpecified(a) || !fPhrases(a)) {
 				// 2025-03-18 https://chatgpt.com/c/67d984ba-4f08-8003-a6dc-e3d70688ceab
 				a.style.display = 'none';
 			}
