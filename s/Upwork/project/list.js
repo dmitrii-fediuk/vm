657e8353c25d997ee2dc40ea9cc235eea32bd7eb
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-02 08:04:10 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list.js
+++ s/Upwork/project/list.js
@@ -464,7 +464,7 @@ GM_addStyle([
 		// 2) https://claude.ai/chat/a79fe12b-9d97-4a6b-87fb-304eb27c0807
 		// 3) https://grok.com/chat/1f2223eb-d192-4d35-a49a-9bcc8729e1b4
 		return c => c.querySelectorAll('article').forEach(a => {
-			if (!fRate(a) || !fRateNotSpecified(a) || !fPhrases(a)) {
+			if (!dfCountries(a) || !fRate(a) || !fRateNotSpecified(a) || !fPhrases(a)) {
 				// 2025-03-18 https://chatgpt.com/c/67d984ba-4f08-8003-a6dc-e3d70688ceab
 				a.style.display = 'none';
 			}
