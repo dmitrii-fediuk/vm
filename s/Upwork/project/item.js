a225da9338093328000e97fb53abf0a2402ae3a5
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-03 01:56:08 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/item.js
+++ s/Upwork/project/item.js
@@ -370,7 +370,6 @@ modify(`[data-qa='client-job-posting-stats']`, i => {// language=Javascript
 // language=CSS
 modify(`ul.client-activity-items > li`, i => {// language=Javascript
 	if (i.textContent.trim().includes('Hires:')) {
-		debugger;
 		document.querySelector('body').classList.toggle(df_c_hired,
 			!!i.querySelector('.value').textContent.match(/\d+/)
 		);
