6960d662027a92b1e5ca5e40ec28d20880c1e90f
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-05 16:52:00 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

---
+++ s/Upwork/project/list/1.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Upwork/project/list/1.js
+// @icon https://www.upwork.com/favicon.ico
+// @match *://www.upwork.com/nx/search/jobs*
+// @name Upwork / Project / List / 1
+// ==/UserScript==
+// 2025-10-05 https://g.co/gemini/share/a3e668c14254
