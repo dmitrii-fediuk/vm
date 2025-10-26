a8ac0b242057cbb18482daa421cc65a4066a4dcc
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-26 16:57:08 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list/countries.js
+++ s/Upwork/project/list/countries.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @grant GM_addStyle
-// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Upwork/project/list/tags.js
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Upwork/project/list/countries.js
 // @icon https://www.upwork.com/favicon.ico
 // @match *://www.upwork.com/nx/search/jobs*
-// @name Upwork / Project / List / Tags
+// @name Upwork / Project / List / Countries
 // ==/UserScript==
 // 2025-10-05
 // 1) https://g.co/gemini/share/ede314ebdbc2
