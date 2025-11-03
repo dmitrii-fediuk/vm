dad4c051f59c95a99d54693c30b75eea1587838c
Dmitrii Fediuk <dfediuk@gmail.com>
2025-11-03 23:13:31 +0000

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini/share.js
+++ s/Google/Gemini/share.js
@@ -27,7 +27,7 @@ GM_addStyle([
 	,'share-viewer ~ *'
 	,'top-bar-actions' // 2025-04-21
 	,'user-notice' // 2025-11-04
-	,'share-turn-viewer' // 2025-11-04
+	,'share-turn-viewer:has(deep-research-confirmation-widget)' // 2025-11-04
 ]
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
