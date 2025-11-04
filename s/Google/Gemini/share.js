42312fba175ac87eb43b0b511ae8eee09dae306f
Dmitrii Fediuk <dfediuk@gmail.com>
2025-11-04 00:36:02 +0000

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini/share.js
+++ s/Google/Gemini/share.js
@@ -26,6 +26,7 @@ GM_addStyle([
 	,'.response-container-header' // 2025-11-04
 	,'.share-viewer_chat-container ~ *'
 	,'.title-container' // 2025-11-04
+	,':has(> .immersive-artifact-container) > message-content' // 2025-11-04
 	,'[data-mat-icon-name="travel_explore"]' // 2025-11-04
 	,'button[mattooltip="Report"]' // 2025-04-21
 	,'deep-research-confirmation-widget' // 2025-11-04
