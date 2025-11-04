d063383e2f7984abc0ec0a16d3366925e7cf156e
Dmitrii Fediuk <dfediuk@gmail.com>
2025-11-04 00:07:12 +0000

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini/chat-share.js
+++ s/Google/Gemini/chat-share.js
@@ -210,3 +210,15 @@ GM_addStyle([
 		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
 	'}'
 );
+// 2025-11-04
+// language=CSS
+GM_addStyle('h1 {font-size: 1.75rem !important; margin: 0 0 .35rem 0 !important;}');
+// 2025-11-04
+// language=CSS
+GM_addStyle('h2 {font-size: 1.5rem !important; margin: .3rem 0 !important;}');
+// 2025-11-04
+// language=CSS
+GM_addStyle('h3 {font-size: 1.25rem !important; margin: .25rem 0 !important;}');
+// 2025-11-04
+// language=CSS
+GM_addStyle('h4 {font-size: 1rem !important; margin: .2rem 0 !important;}');
