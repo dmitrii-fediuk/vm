f08f274f7108872051b00024ebd9525e63f96ae5
Dmitrii Fediuk <dfediuk@gmail.com>
2025-11-04 08:15:03 +0000

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini/share.js
+++ s/Google/Gemini/share.js
@@ -129,6 +129,15 @@ GM_addStyle([
 	.join(`,`) + `{height: auto !important;}`)
 ;
 
+// 2025-11-04
+// language=CSS
+GM_addStyle([
+	`.source-card-footer` // 2025-11-04
+]
+	 // language=Javascript
+	.join(',') + '{max-height: auto !important;}')
+;
+
 // 2025-04-20
 // language=CSS
 GM_addStyle([
