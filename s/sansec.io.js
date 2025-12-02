5ae3b55948c45f98de8f860be686383486339aa1
Dmitrii Fediuk <dfediuk@gmail.com>
2025-12-02 14:48:15 +0000

    https://github.com/dmitrii-fediuk/vm/issues/120

---
+++ s/sansec.io.js
@@ -0,0 +1,374 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/sansec.io.js
+// @icon https://sansec.io/assets/images/sansec-social.svg
+// @match https://sansec.io/*
+// @name sansec.io
+// ==/UserScript==
+// 2025-12-02 "Improve `sansec.io`": https://github.com/dmitrii-fediuk/vm/issues/120
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{all: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+	'.text' // 2025-12-02
+	,'p' // 2025-12-02
+]
+	 // language=Javascript
+	.join(',') + '{all: revert !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+	'#adbMsgClientWrapper' // 2025-12-02
+	,'#root_content_flex > div > .position:not(:first-of-type)' // 2025-12-02
+	,'.globalNavFooter' // 2025-12-02
+	,'.globalnavheader' // 2025-12-02
+	,'.helpxFooter' // 2025-12-02
+	,'.horizontalRule + .aem-GridColumn:has(a[href$="helpx.adobe.com/security.html"])' // 2025-12-02
+	,'.horizontalRule' // 2025-12-02
+	,'.note-icon' // 2025-12-02
+	,'.titleBar' // 2025-12-02
+	,'.toc' // 2025-12-02
+	,'main ~ .feedback' // 2025-12-02
+	,'p > br:first-child' // 2025-12-02
+	,'p:has(a[href$="https://hackerone.com/adobe"])' // 2025-12-02
+]
+	 // language=Javascript
+	.join(',') + '{display: none !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+	'#root_content_flex > div' // 2025-12-02
+]
+	 // language=Javascript
+	.join(',') + '{display: block !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		Object.entries({
+			// language=CSS
+			'display': 'flex'
+			,'flex-direction': 'row'
+			,'flex-wrap': 'wrap'
+		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
+	'}'
+);
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{position: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+	'#root_content_flex' // 2025-12-02
+	,'.page-description' // 2025-12-02
+]
+	 // language=Javascript
+	.join(',') + '{margin: 0 !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+	'.helpx-note' // 2025-12-02
+	,'p' // 2025-12-02
+]
+	 // language=Javascript
+	.join(',') + '{margin: 0.25rem 0 !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+	'#root_content_flex:not(#A)' // 2025-12-02
+	,'.applies-to-container:not(#A)' // 2025-12-02
+	,'.helpx-note' // 2025-12-02
+	,'.position' // 2025-12-02
+	,'h2' // 2025-12-02
+]
+	 // language=Javascript
+	.join(',') + '{padding: 0 !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{align-items: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{background-color: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+	'.helpx-note' // 2025-12-02
+	,'h2' // 2025-12-02
+]
+	 // language=Javascript
+	.join(',') + '{border: 0 !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{border-radius: 0 !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{box-shadow: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{color: initial !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{clear: both !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{flex-basis: 100%; !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{float: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{font-family: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{font-size: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{font-weight: revert !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{gap: 0 !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{height: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{line-height: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{line-height: .9 !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{min-height: unset !important;}')
+;
+
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{overflow-x: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{table-layout: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{text-align: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{top: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+	'td' // 2025-12-02
+]
+	 // language=Javascript
+	.join(',') + '{width: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+	'#root_content_flex > div > .position:first-of-type' // 2025-12-02
+	,'table' // 2025-12-02
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		['max-width', 'width', 'min-width']
+			// language=Javascript
+			.map(k => `${k}: 100% !important;`).join(' ') +
+	'}'
+);
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		Object.entries({
+			'height': 'auto'
+			// language=Javascript
+			,'width': '100%'
+		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
+	'}'
+);
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		Object.entries({
+			'font-family': 'Consolas'
+			// language=Javascript
+			,'font-size': '1.5rem'
+		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
+	'}'
+);
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	 // language=Javascript
+	.join(',') + '{font-weight: 600 !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+	'body:not(#A)' // 2025-12-02
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		Object.entries({
+			'font-family': 'Segoie UI'
+			// language=Javascript
+			,'font-size': '150%'
+			// language=CSS
+			,'line-height': 1.25
+		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
+	'}'
+);
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+	'div:not(#A)' // 2025-12-02
+	,'li' // 2025-12-02
+	,'p' // 2025-12-02
+	,'span:not(#A)' // 2025-12-02
+	,'table' // 2025-12-02
+]
+	 // language=Javascript
+	.join(',') + '{font-size: unset !important;}')
+;
+// 2025-12-02
+// language=CSS
+GM_addStyle([
+]
+	// language=Javascript
+	.join(',') + '{' +
+		// language=CSS
+		['font-size', 'font-weight', 'line-height']
+			// language=Javascript
+			.map(k => `${k}: unset !important;`).join(' ') +
+	'}'
+);
+// 2025-12-02
+// language=CSS
+GM_addStyle('body {margin: 1rem .5rem !important;}');
+// 2025-12-02
+// language=CSS
+GM_addStyle('h2 {margin: .5rem 0 !important;}');
+// 2025-12-02 https://gemini.google.com/share/814d8f5fd665
+document.querySelectorAll('p').forEach(p => {
+	if (!p.children.length && !/\S/.test(p.textContent)) {
+		p.remove();
+	}
+});
+// 2025-12-02
+// https://gemini.google.com/share/4bc89b41bad9
+// language=CSS
+//GM_addStyle('table {table-layout: fixed; !important;}');
+// 2025-12-02
+// 1) https://gemini.google.com/share/4bc89b41bad9
+// 2) https://gemini.google.com/share/78237c927526
+// language=CSS
+GM_addStyle('td {overflow-wrap: anywhere; !important; min-width: 8ch !important;}');
