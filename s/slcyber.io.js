07baed8d091be60e1c3d066a5d01b72e9d24fc02
Dmitrii Fediuk <dfediuk@gmail.com>
2025-12-02 18:46:56 +0000

    https://github.com/dmitrii-fediuk/vm/issues/121

--- s/slcyber.io.js
+++ s/slcyber.io.js
@@ -119,6 +119,7 @@ GM_addStyle([
 // language=CSS
 GM_addStyle([
 	'body:not(#A)' // 2025-12-02
+	,'code:not(#A)' // 2025-12-02
 ]
 	 // language=Javascript
 	.join(',') + '{background-color: unset !important;}')
