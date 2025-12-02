95e8df20d00254f5e1d5b0e35480ff386db927c6
Dmitrii Fediuk <dfediuk@gmail.com>
2025-12-02 15:08:09 +0000

    https://github.com/dmitrii-fediuk/vm/issues/120

--- s/sansec.io.js
+++ s/sansec.io.js
@@ -294,6 +294,7 @@ GM_addStyle([
 // 2025-12-02
 // language=CSS
 GM_addStyle([
+	'body'
 ]
 	// language=Javascript
 	.join(',') + '{' +
@@ -328,7 +329,7 @@ GM_addStyle([
 );
 // 2025-12-02
 // language=CSS
-//GM_addStyle('body {margin: 1rem .5rem !important;}');
+GM_addStyle('body {margin: 1rem .5rem !important;}');
 // 2025-12-02
 // language=CSS
 //GM_addStyle('h2 {margin: .5rem 0 !important;}');
