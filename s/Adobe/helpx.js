587f8e844af2eb546b64f7f1714cf869379b5dbc
Dmitrii Fediuk <dfediuk@gmail.com>
2025-12-02 11:18:46 +0000

    https://github.com/dmitrii-fediuk/vm/issues/119

--- s/Adobe/helpx.js
+++ s/Adobe/helpx.js
@@ -1,46 +1,41 @@
 // ==UserScript==
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @grant GM_addStyle
-// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Alignet/docs.js
-// @icon https://docs.alignet.com/__assets-5ae8127f-cd07-4738-b1e9-d974e1d377dc/image/Recurso%201@4x.png
-// @match https://docs.alignet.com/*
-// @name Alignet / Docs
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Adobe/helpx.js
+// @icon https://experienceleague.adobe.com/favicon.ico
+// @match https://helpx.adobe.com/*
+// @name Adobe / HelpX
 // ==/UserScript==
-// 2025-10-10 "Improve `alignet.com`": https://github.com/dmitrii-fediuk/vm/issues/118
-// 2025-10-10
+// 2025-12-02 "Improve `helpx.adobe.com`": https://github.com/dmitrii-fediuk/vm/issues/119
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{all: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{all: revert !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
-	'#vp-js-mobile__navigation' // 2025-10-10
-	,'footer' // 2025-10-10
-	,'header' // 2025-10-10
-	,'vp-article-pagination' // 2025-10-10
 ]
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
-	'#main-content' // 2025-10-10
 ]
 	 // language=Javascript
 	.join(',') + '{display: block !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
@@ -55,189 +50,182 @@ GM_addStyle([
 		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
 	'}'
 );
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{position: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
-	'#main-content' // 2025-10-10
-	,'.table-wrap' // 2025-10-10
 ]
 	 // language=Javascript
 	.join(',') + '{margin: 0 !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
-	'#article-inner-content' // 2025-10-10
-	,'.panel-macro' // 2025-10-10
-	,'.table-wrap' // 2025-10-10
-	,'main' // 2025-10-10
 ]
 	 // language=Javascript
 	.join(',') + '{padding: 0 !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{align-items: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{background-color: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{border: 0 !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{border-radius: 0 !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{box-shadow: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{color: initial !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{clear: both !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{flex-basis: 100%; !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{float: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{font-family: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{font-size: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{font-weight: revert !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
-	'main' // 2025-10-10
 ]
 	 // language=Javascript
 	.join(',') + '{gap: 0 !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{height: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{line-height: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{line-height: .9 !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{min-height: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{overflow-x: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{table-layout: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{text-align: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{top: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{width: unset !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
@@ -249,7 +237,7 @@ GM_addStyle([
 			.map(k => `${k}: 100% !important;`).join(' ') +
 	'}'
 );
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
@@ -263,7 +251,7 @@ GM_addStyle([
 		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
 	'}'
 );
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
@@ -277,14 +265,14 @@ GM_addStyle([
 		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
 	'}'
 );
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
 	 // language=Javascript
 	.join(',') + '{font-weight: 600 !important;}')
 ;
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
@@ -300,7 +288,7 @@ GM_addStyle([
 		}).map(([k, v]) => `${k}: ${v} !important;`).join(' ') +
 	'}'
 );
-// 2025-10-10
+// 2025-12-02
 // language=CSS
 GM_addStyle([
 ]
@@ -312,9 +300,9 @@ GM_addStyle([
 			.map(k => `${k}: unset !important;`).join(' ') +
 	'}'
 );
-// 2025-10-10
+// 2025-12-02
 // language=CSS
-GM_addStyle('body {margin: 0 1rem !important;}');
-// 2025-10-10
+//GM_addStyle('body {margin: 0 1rem !important;}');
+// 2025-12-02
 // language=CSS
-GM_addStyle('h3 {margin: 0.25rem 0 !important;}');
+//GM_addStyle('h3 {margin: 0.25rem 0 !important;}');
