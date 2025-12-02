41459b66b47cd36181abf72f16e5635ed1698f84
Dmitrii Fediuk <dfediuk@gmail.com>
2025-12-02 12:09:56 +0000

    https://github.com/dmitrii-fediuk/vm/issues/119

--- s/Adobe/helpx.js
+++ s/Adobe/helpx.js
@@ -11,8 +11,7 @@
 // language=CSS
 GM_addStyle([
 	'.text' // 2025-12-02
-	,'p'
-	,'table'
+	,'p' // 2025-12-02
 ]
 	 // language=Javascript
 	.join(',') + '{all: unset !important;}')
@@ -327,6 +326,7 @@ GM_addStyle([
 GM_addStyle([
 	'div:not(#A)' // 2025-12-02
 	,'p' // 2025-12-02
+	,'table' // 2025-12-02
 ]
 	 // language=Javascript
 	.join(',') + '{font-size: unset !important;}')
