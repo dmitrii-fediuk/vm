3608541a9b1773f226c0099ca05f6d78ee1738ce
Dmitrii Fediuk <dfediuk@gmail.com>
2025-12-16 00:06:49 +0000

    https://github.com/dmitrii-fediuk/vm/issues/122

--- s/Google/issues.chromium.org.js
+++ s/Google/issues.chromium.org.js
@@ -27,6 +27,7 @@ GM_addStyle([
 	':has(> b-pagination-one-issue)' // 2025-12-16
 	,':has(> issue-chip-indicators)' // 2025-12-16
 	,'b-footer' // 2025-12-16
+	,'b-hiring-banner' // 2025-12-16
 	,'b-tracker-banner' // 2025-12-16
 	,'header.main-header' // 2025-12-16
 	,'issue-title ~ *' // 2025-12-16
