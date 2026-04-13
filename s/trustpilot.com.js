0257028011b34c7a1f0d1ca3d9a1c364f55ff034
Dmitrii Fediuk <dfediuk@gmail.com>
2026-04-13 17:04:01 +0100

    https://github.com/dmitrii-fediuk/vm/issues/125

--- s/trustpilot.com.js
+++ s/trustpilot.com.js
@@ -53,6 +53,7 @@ GM_addStyle([
 	,`[class^='CDS_BannerAlert']` // 2026-04-13
 	,`[class^='styles_banners']` // 2026-04-13
 	,`[class^='styles_businessInfoSideBar']` // 2026-04-13
+	,`[class^='styles_helpCenterLink']` // 2026-04-13
 	,`[class^='styles_stickyColumnContent']` // 2026-04-13
 	,`[class^='styles_summaryContainer']` // 2026-04-13
 	,`[class^='styles_trustpilotExperienceCardContainer']` // 2026-04-13
