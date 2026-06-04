05839dfce0a21bb4cd5b7fd66a5b9aa5b353979b
Dmitrii Fediuk <dfediuk@gmail.com>
2026-06-04 19:27:19 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list/best-matches/.js
+++ s/Upwork/project/list/best-matches/.js
@@ -123,9 +123,9 @@ GM_addStyle([
 // 2026-06-04
 // language=CSS
 GM_addStyle([
-	`${dfJobTileBadges}` // 2026-06-04
-	,`.job-tile-actions` // 2026-06-04
+	`.job-tile-actions` // 2026-06-04
 	,`.job-tile-toggle` // 2026-06-04
+	,dfJobTileBadges // 2026-06-04
 ]
 	 // language=Javascript
 	.join(',') + '{opacity: .5 !important;}')
