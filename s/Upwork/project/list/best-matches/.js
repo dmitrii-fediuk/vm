77fdbed7cedb09e3645bdc52d1a3ca708b8d091d
Dmitrii Fediuk <dfediuk@gmail.com>
2026-06-13 00:49:05 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list/best-matches/.js
+++ s/Upwork/project/list/best-matches/.js
@@ -341,9 +341,11 @@ modify(dfText, i => {// language=Javascript
 	// 1) https://stackoverflow.com/a/784547
 	// 2.1) https://claude.ai/chat/5d359891-1f1d-400f-bc3b-e8b30fa92867
 	// 2.2) https://chatgpt.com/c/67d9e839-2800-8003-9a50-5a9edd88c3a2
-	let v = i.textContent.replace(/(?:\r\n|\r|\n)/g, '<br/>');
-	v = v.replaceAll(`<br/><br/>`, `<div class='${dfNL}'/>`) // 2026-06-13
-	i.innerHTML = v;
+	const br = '<br/>';
+	i.innerHTML = i.textContent
+		.replace(/(?:\r\n|\r|\n)/g, br)
+		.replaceAll(`${br}${br}`, `<div class='${dfNL}'/>`) // 2026-06-13
+	;
 });
 // 2026-06-12
 // language=CSS
