a41a3472c53e7d89222885f56ced4f7e31b794c4
Dmitrii Fediuk <dfediuk@gmail.com>
2026-06-12 22:54:59 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list/best-matches/.js
+++ s/Upwork/project/list/best-matches/.js
@@ -33,13 +33,13 @@ const dfJobsFeed = `[data-test='jobs-feed']`; // 2026-06-04
 // language=CSS
 const dfPost = `section[data-ev-sublocation='job_feed_tile']`; // 2026-06-12
 // language=CSS
-const dfPost__Body = `${dfPost} > div:nth-of-type(2)`; // 2026-06-12
+const dfProject__Body = `${dfPost} > div:nth-of-type(2)`; // 2026-06-12
 // language=CSS
-const dfPost__Body__Row_Country = `.badge-line`; // 2026-06-12
+const dfProject__Body__Row_Country = `.badge-line`; // 2026-06-12
 // language=CSS
-const dfPost__Header = `${dfPost} > div:nth-of-type(1)`; // 2026-06-12
+const dfProject__Header = `${dfPost} > div:nth-of-type(1)`; // 2026-06-12
 // language=CSS
-const dfPost__Header__Row_PostedOn = `${dfPost__Header} > div:first-of-type`; // 2026-06-12
+const dfProject__Header__Row_PostedOn = `${dfProject__Header} > div:first-of-type`; // 2026-06-12
 // language=CSS
 const dfPostedOnContainer = `span:has(> ${df_DT_PostedOn})`; // 2026-06-12
 // language=CSS
@@ -66,10 +66,10 @@ setTimeout(() => {
 		,`${df_C_FeedsCard} > :not(:has(> ${df_DT_FeedBestMatch}))` // 2026-06-12
 		,`${df_C_JobTileBadges} > :not([data-test='applied-badge'])` // 2026-06-12
 		,`${df_DT_FeedBestMatch} > :not(:has(> ${df_DT_JobTileList}))` // 2026-06-12
-		,`${dfPost__Body__Row_Country} > :not(${df_DT_Country})` // 2026-06-12
-		,`${dfPost__Body__Row_Country} .air3-icon` // 2026-06-12
-		,`${dfPost__Body__Row_Country} ~ *` // 2026-06-12
-		,`${dfPost__Header__Row_PostedOn} > .text-caption > span:not(${dfPostedOnContainer})` // 2026-06-12
+		,`${dfProject__Body__Row_Country} > :not(${df_DT_Country})` // 2026-06-12
+		,`${dfProject__Body__Row_Country} .air3-icon` // 2026-06-12
+		,`${dfProject__Body__Row_Country} ~ *` // 2026-06-12
+		,`${dfProject__Header__Row_PostedOn} > .text-caption > span:not(${dfPostedOnContainer})` // 2026-06-12
 		,`.featured-tag:not(#a)` // 2026-06-04
 		,`.impression-tracker` // 2026-06-04
 		,`.job-tile-actions > :not([data-test='job-feedback'])` // 2026-06-04
@@ -107,9 +107,9 @@ GM_addStyle([
 	,`${df_Ev_BestMatch}:not(#a)` // 2026-06-04
 	,`.app-frame` // 2026-06-12
 	,df_C_FeedsCard // 2026-06-12
-	,dfPost__Body__Row_Country // 2026-06-12
-	,dfPost__Header // 2026-06-12
-	,dfPost__Header__Row_PostedOn // 2026-06-12
+	,dfProject__Body__Row_Country // 2026-06-12
+	,dfProject__Header // 2026-06-12
+	,dfProject__Header__Row_PostedOn // 2026-06-12
 	,dfText // 2026-06-04
 ]
 	 // language=Javascript
@@ -202,7 +202,7 @@ GM_addStyle([
 // language=CSS
 GM_addStyle([
 	`${dfPost}:not(#a)` // 2026-06-04
-	,`${dfPost__Header} > div` // 2026-06-04
+	,`${dfProject__Header} > div` // 2026-06-04
 	,`:has(> ${df_DT_JobTileBadges}):not(#a)` // 2026-06-04
 ]
 	 // language=Javascript
@@ -252,7 +252,7 @@ GM_addStyle('body {padding: 0.5rem !important;}');
 // 2026-06-12
 // language=CSS
 GM_addStyle([
-	dfPost__Body // 2026-06-12
+	dfProject__Body // 2026-06-12
 // language=Javascript
 ].join(',') + `{${Object.entries({
 	'display': 'flex'
@@ -261,15 +261,15 @@ GM_addStyle([
 }).map(v => `${v[0]}: ${v[1]} !important;`).join(' ')}}`);
 // 2026-06-12
 // language=CSS
-GM_addStyle(`${dfPost__Body} > :not(${dfPost__Body__Row_Country}) {order: 1 !important;}`);
+GM_addStyle(`${dfProject__Body} > :not(${dfProject__Body__Row_Country}) {order: 1 !important;}`);
 // 2026-06-12 https://gemini.google.com/share/63decfb2de6f
 // language=CSS
-GM_addStyle(`${dfPost__Body} > :not(${dfPost__Body__Row_Country}, :has(${df_DT_JobType})) {
+GM_addStyle(`${dfProject__Body} > :not(${dfProject__Body__Row_Country}, :has(${df_DT_JobType})) {
 	flex-basis: 100% !important;
 }`);
 // 2026-06-12
 // language=CSS
-//GM_addStyle(`${dfPost__Body__Row_Country}:not(#a) {order: 1 !important;}`);
+//GM_addStyle(`${dfProject__Body__Row_Country}:not(#a) {order: 1 !important;}`);
 // 2026-06-04
 // language=CSS
 GM_addStyle(`${dfTitle} {font-size: 1.25rem !important;}`);
