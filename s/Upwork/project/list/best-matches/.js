afd0f4a48c4e34bb5991914fbae1a96392e8df47
Dmitrii Fediuk <dfediuk@gmail.com>
2026-06-12 18:53:42 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list/best-matches/.js
+++ s/Upwork/project/list/best-matches/.js
@@ -47,7 +47,7 @@ setTimeout(() => {
 	GM_addStyle([
 		`${dfTabs} > :not(${dfTabContent}):not(#a)` // 2026-06-04
 		,`${dfJobsFeed} > :not(${dfTabs})` // 2026-06-04
-		,`${df_DT_FeedBestMatch} > :not(${df_Ev_BestMatch})` // 2026-06-04
+		//,`${df_DT_FeedBestMatch} > :not(${df_Ev_BestMatch})` // 2026-06-04
 		,`${df_Row_Country} > :not(:last-child):not(#a)` // 2026-06-04
 		,`${df_Row_Country} [data-test='icon']` // 2026-06-04
 		,`${df_Row_Header_3} > :not(:first-child)` // 2026-06-04
