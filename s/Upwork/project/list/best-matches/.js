55f2f6ec7c0d525ff311df3e89925e6c9e900d25
Dmitrii Fediuk <dfediuk@gmail.com>
2026-06-12 23:51:52 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list/best-matches/.js
+++ s/Upwork/project/list/best-matches/.js
@@ -134,7 +134,8 @@ GM_addStyle([
 // 2026-06-12
 // language=CSS
 GM_addStyle([
-	`:hover:not(#A)` // 2026-05-04
+	`.visited` // 2026-06-13
+	,`:hover:not(#A)` // 2026-05-04
 	,dfPostedOnContainer_Parent // 2026-06-12
 ]
 	 // language=Javascript
