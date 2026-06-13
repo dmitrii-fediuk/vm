d2e0f0177e82674c27f9ccc551d4cf90cc1eeb4f
Dmitrii Fediuk <dfediuk@gmail.com>
2026-06-13 04:44:16 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list/normal/.js
+++ s/Upwork/project/list/normal/.js
@@ -9,6 +9,8 @@
 // 2024-10-13, 2024-12-24 "Improve the Upwork appearance": https://github.com/dmitrii-fediuk/vm/issues/52
 const ᛡ = unsafeWindow.df.upwork.project.list; // 2025-10-26
 const dfNL = `df-nl`; // 2026-06-13
+// language=CSS
+const dfProject = `article[data-test='JobTile']`; // 2026-06-13
 // 2025-06-06
 // 1) «Failed to execute 'appendChild' on 'Node': This node type does not support this method».
 // 2) https://g.co/gemini/share/647370d4f366
