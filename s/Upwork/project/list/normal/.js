05327995d3e22893c6ef437da286a5e0fe94b9d4
Dmitrii Fediuk <dfediuk@gmail.com>
2026-06-13 01:13:34 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list/normal/.js
+++ s/Upwork/project/list/normal/.js
@@ -260,10 +260,11 @@ GM_addStyle([
 			if (p) {
 				const br = '<br/>';
 				p.innerHTML = p.textContent
-					.replace(/(?:\r\n|\r|\n)/g, br) // 2025-03-19 https://stackoverflow.com/a/784547
-					.replaceAll('<br>', br)
+					// 2025-03-19 https://stackoverflow.com/a/784547
+					// 2026-06-13 https://gemini.google.com/share/8660a99264ca
+					.replace(/\r?\n|\r|<br>/g, br)
 					// 2026-06-13 https://gemini.google.com/share/bceb5af402b2
-					.replace(new RegExp(`(?:${br}){2,}`, 'g'), `<div class='${dfNL}'></div>`)
+					.replace(/(?:<br\/>){2,}/g, `<div class='${dfNL}'></div>`)
 				;
 			}
 		};
