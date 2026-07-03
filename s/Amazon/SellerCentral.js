7fbc2ca73da0776e30f4dc52eed9b9abf1a5ca8e
Dmitrii Fediuk <dfediuk@gmail.com>
2026-07-03 23:26:32 +0100

    https://github.com/dmitrii-fediuk/vm/issues/129

--- s/Amazon/SellerCentral.js
+++ s/Amazon/SellerCentral.js
@@ -42,7 +42,8 @@ GM_addStyle([
 // 2026-07-04
 // language=CSS
 GM_addStyle([
-	`.lego-header` // 2026-07-04
+	`.hh-article-country-box` // 2026-07-04
+	, `.lego-header` // 2026-07-04
 ]
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
