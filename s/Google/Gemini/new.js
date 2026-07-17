ace0421572f2d3295687b0efb491e39090eba15b
Dmitrii Fediuk <dfediuk@gmail.com>
2026-07-17 07:36:53 +0100

    https://github.com/dmitrii-fediuk/vm/issues/60

--- s/Google/Gemini/new.js
+++ s/Google/Gemini/new.js
@@ -56,4 +56,6 @@ GM_addStyle([
 ;
 // 2026-07-17
 // language=CSS
-GM_addStyle(`chat-window-content {background: yellow; display: block; height: 25% !important;}`);
+GM_addStyle(`chat-window-content {
+	background: yellow; display: block !important; height: 25% !important;
+}`);
