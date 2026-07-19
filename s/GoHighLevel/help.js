1e7e074e28f5458a49a252ae661787977e7e82fd
Dmitrii Fediuk <dfediuk@gmail.com>
2026-07-19 18:51:25 +0100

    https://github.com/dmitrii-fediuk/vm/issues/133

--- s/GoHighLevel/help.js
+++ s/GoHighLevel/help.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Shopify/help.js
 // @icon https://cdn.shopify.com/b/shopify-brochure2-assets/c97c60ca19c64a8b5378d9f9e971f7bd.png
 // @match https://help.shopify.com/*
-// @name Shopify / Help
+// @name GoHighLevel / Help
 // ==/UserScript==
 // 2026-07-19 "Improve `gohighlevel.com`": https://github.com/dmitrii-fediuk/vm/issues/133
 // language=CSS
