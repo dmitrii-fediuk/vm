ea46df9917eb428a19c3fb3bc267ef1d306df570
Dmitrii Fediuk <dfediuk@gmail.com>
2026-07-19 19:28:24 +0100

    https://github.com/dmitrii-fediuk/vm/issues/133

--- s/GoHighLevel/help.js
+++ s/GoHighLevel/help.js
@@ -106,7 +106,7 @@ GM_addStyle([
 // 2026-07-19
 // language=CSS
 GM_addStyle([
-	`h3`, `ol`, `p`, `ul` // 2026-07-19
+	`:is(h3, ol, p, ul):not(#a)` // 2026-07-19
 ]
 	 // language=Javascript
 	.join(',') + '{margin: .25rem 0 !important;}')
