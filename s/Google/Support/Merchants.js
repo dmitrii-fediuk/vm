8f2feaaa32195199031c69ee935a8572a71f64ec
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-06 10:08:57 +0100

    https://github.com/dmitrii-fediuk/vm/issues/87

--- s/Google/Support/Merchants.js
+++ s/Google/Support/Merchants.js
@@ -13,6 +13,7 @@ GM_addStyle([
 	,`.scTailwindThreadThreadcontenttitle-back` // 2026-08-06
 	,`:has(> .scTailwindThreadQuestionQuestioncarddisclaimer)` // 2026-08-06
 	,`aside` // 2026-08-06
+	,`sc-tailwind-thread-thread-alert` // 2026-08-06
 	,`sc-tailwind-thread-question-question-actions` // 2026-08-06
 ]
 	 // language=Javascript
