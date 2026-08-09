98ae49a3f110f12daae9845c8d41e8a09e550f20
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-09 12:52:55 +0100

    https://github.com/dmitrii-fediuk/vm/issues/135

--- s/Amazon/Marketplace/Product.js
+++ s/Amazon/Marketplace/Product.js
@@ -46,6 +46,7 @@ GM_addStyle([
 // language=CSS
 GM_addStyle([
 	`#above-dp-container` // 2026-08-09
+	,`#rhf` // 2026-08-09
 	,`:has(> iframe)` // 2026-08-09
 	,`[data-feature-name='nile-inline-btf']` // 2026-08-09
 	,`[data-feature-name='sims-discoveryAndInspiration']` // 2026-08-09
