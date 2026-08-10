9fe4b2edbe06b82354528cb7243e2c6b4cc96317
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-10 19:50:21 +0100

    https://github.com/dmitrii-fediuk/vm/issues/131

--- s/Meta/Business/Help.js
+++ s/Meta/Business/Help.js
@@ -102,6 +102,7 @@ GM_addStyle([
 GM_addStyle([
 	`#FB4BResponsiveMainContent > *` // 2026-08-10
 	,`#GBMBizEdArticleRoot` // 2026-08-10
+	,`#faq-page div` // 2026-08-10
 ]
 	 // language=Javascript
 	.join(',') + '{padding: 0 !important;}')
