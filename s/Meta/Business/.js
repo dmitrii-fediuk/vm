0b581fc778edd93499b99573580c9961181a29d8
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-10 19:22:41 +0100

    https://github.com/dmitrii-fediuk/vm/issues/131

--- s/Meta/Business/.js
+++ s/Meta/Business/.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @grant GM_addStyle
-// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Meta/Business.js
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Meta/Business/.js
 // @icon https://business.facebook.com/images/bizkit/mbs_favicon.png
 // @match https://business.facebook.com/*
 // @name Meta / Business
