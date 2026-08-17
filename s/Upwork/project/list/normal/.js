d4f1414f02d52aadcdb4960e18d9f6143d6046b9
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-17 19:49:46 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list/normal/.js
+++ s/Upwork/project/list/normal/.js
@@ -102,8 +102,9 @@ GM_addStyle([
 // 2026-05-04
 // language=CSS
 GM_addStyle([
-	`.visited` // 2026-06-13
+	,`.visited` // 2026-06-13
 	,`:hover:not(#A)` // 2026-05-04
+	,df_DT_Date // 2026-08-17
 ]
 	 // language=Javascript
 	.join(',') + '{background-color: unset !important;}')
