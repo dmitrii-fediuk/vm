3ee56e7dd1a7caf62074e3e58c1bab5d526bd00e
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-17 21:19:41 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list/best-matches/nx-s.js
+++ s/Upwork/project/list/best-matches/nx-s.js
@@ -75,6 +75,10 @@ GM_addStyle([
 GM_addStyle([
 	`${dfJobTileBadges}:not(#a)` // 2026-06-04
 	,`${df_Ev_BestMatch}:not(#a)` // 2026-06-04
+	,`[class*=mb-]` // 2026-08-17
+	,`[class*=mt-]` // 2026-08-17
+	,`[class*=mx-]` // 2026-08-17
+	,`[class*=my-]` // 2026-08-17
 	,dfText // 2026-06-04
 ]
 	 // language=Javascript
