c407e4ce3ceaa0194c7115f802d86990a6c55a30
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-23 05:50:17 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/common.js
+++ s/Upwork/common.js
@@ -34,6 +34,14 @@ setTimeout(() => {
 		.join(',') + '{display: none !important;}')
 	;
 }, 200);
+// 2026-08-23
+// language=CSS
+GM_addStyle([
+	`.layout:has(> .main-content-container)` // 2026-08-23
+]
+	 // language=Javascript
+	.join(',') + '{display: block !important;}')
+;
 // language=CSS
 GM_addStyle([
 	'[role="alert"]'
