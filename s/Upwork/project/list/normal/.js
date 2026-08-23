86a493c15d539b77edb406015e98b3624663c0a4
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-23 08:01:36 +0100

    https://github.com/dmitrii-fediuk/vm/issues/136

--- s/Upwork/project/list/normal/.js
+++ s/Upwork/project/list/normal/.js
@@ -228,10 +228,8 @@ GM_addStyle([
 	dfText // 2025-03-19
 // language=Javascript
 ].join(',') + `{${Object.entries({
-	//'font-family': 'Segoe UI'
 	'font-size': '120%'
 	,'line-height': 1.2
-	//,'letter-spacing': '.03em' // 2026-05-04
 }).map(v => `${v[0]}: ${v[1]} !important;`).join(' ')}}`);
 // 2026-06-15
 if (true) {
