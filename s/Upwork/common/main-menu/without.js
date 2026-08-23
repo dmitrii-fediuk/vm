1528e58c2333be07ca790f15c3f73c82b442497f
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-23 06:27:18 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/common/main-menu/without.js
+++ s/Upwork/common/main-menu/without.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
 // @grant GM_addStyle
-// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Upwork/common/wo-sidebar.js
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Upwork/common/main-menu/without.js
 // @icon https://www.upwork.com/favicon.ico
 // @match *://www.upwork.com/*
-// @name Upwork / Common / Without Sidebar
+// @name Upwork / Common / Main Menu / Without
 // ==/UserScript==
 // 2026-08-23 "Improve the Upwork appearance": https://github.com/dmitrii-fediuk/vm/issues/52
 if (['/freelancers/mage2pro'].includes(location.pathname)) {
