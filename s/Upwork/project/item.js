3c39ef3004dd5fca74ef483682379ce2df7df0d0
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-23 09:34:26 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/item.js
+++ s/Upwork/project/item.js
@@ -136,16 +136,14 @@ GM_addStyle([
 // language=CSS
 GM_addStyle([
 	`.${df_c_hired}`
-	//,`.${df_c_hired} .air3-card-sections`
-	,`.${df_c_hired} .main-content` // 2026-08-23
+	,`.${df_c_hired} :is(.air3-card-sections, .main-content)` // 2025-10-03, 2026-08-23
 // language=Javascript
 ].join(`,`) + `{background: #ffe6e6 !important;}`);
 // 2025-11-23 https://share.google/TUp89J1Q0llPHWhkI
 // language=CSS
 GM_addStyle([
 	`.${df_c_hired_and_closed}`
-	//,`.${df_c_hired_and_closed} .air3-card-sections`
-	,`.${df_c_hired_and_closed} .main-content` // 2026-08-23
+	,`.${df_c_hired_and_closed} :is(.air3-card-sections, .main-content)` // 2025-11-23, 2026-08-23
 // language=Javascript
 ].join(`,`) + `{background: #f5e9e9 !important;}`);
 // 2025-09-05
