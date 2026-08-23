aa551def1bb4da1e07a17c0f1e51bed81a25a609
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-23 08:20:28 +0100

    https://github.com/dmitrii-fediuk/vm/issues/136

--- s/Upwork/common/.js
+++ s/Upwork/common/.js
@@ -97,9 +97,9 @@ GM_addStyle([
 // language=CSS
 GM_addStyle([
 	//`*` // 2026-08-23
-	`:is(.h1, .h2, .h3, .h4, .h5, .h6)` // 2026-08-23
-	,`p.text-base-sm` // 2026-05-04
-	,`p.text-body-sm` // 2026-05-04
+	`:is(h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6)` // 2026-08-23
+	,`p.text-base-sm:not(#a)` // 2026-05-04
+	,`p.text-body-sm:not(#a)` // 2026-05-04
 // language=Javascript
 ].join(',') + '{letter-spacing: revert !important;}');
 // 2026-08-23
