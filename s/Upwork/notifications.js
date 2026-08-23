6dbbc6012b7f256d652b0256924e2a344a66b68f
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-23 05:56:02 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

---
+++ s/Upwork/notifications.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @author Dmitrii Fediuk (https://upwork.com/fl/mage2pro)
+// @grant GM_addStyle
+// @homepageURL https://github.com/dmitrii-fediuk/vm/blob/main/s/Upwork/notifications.js
+// @icon https://www.upwork.com/favicon.ico
+// @match *://www.upwork.com/ab/notifications/
+// @name Upwork / Notifications
+// ==/UserScript==
+// 2026-08-23 "Improve the Upwork appearance": https://github.com/dmitrii-fediuk/vm/issues/52
+// 2025-06-10
+// 1) «Failed to execute 'appendChild' on 'Node': This node type does not support this method».
+// 2) https://g.co/gemini/share/647370d4f366
+setTimeout(() => {
+	// language=CSS
+	GM_addStyle([
+		`.air3-tabs-header` // 2026-08-23
+	]
+		 // language=Javascript
+		.join(',') + '{display: none !important;}')
+	;
+}, 200);
