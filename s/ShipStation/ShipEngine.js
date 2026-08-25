dd67c5d529f99e15aa0b42432ac580d03ea0884f
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-25 08:06:11 +0100

    https://github.com/dmitrii-fediuk/vm/issues/138

--- s/ShipStation/ShipEngine.js
+++ s/ShipStation/ShipEngine.js
@@ -56,6 +56,7 @@ GM_addStyle([
 // language=CSS
 GM_addStyle([
 	`:has(> #leftGroup)` // 2026-08-25
+	,`:has(> main)` // 2026-08-25
 ]
 	 // language=Javascript
 	.join(',') + '{display: block !important;}')
