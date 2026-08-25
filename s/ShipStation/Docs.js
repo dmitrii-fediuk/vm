b88694322547d578bb055185422fdfccb4edfb09
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-25 06:16:53 +0100

    https://github.com/dmitrii-fediuk/vm/issues/137

--- s/ShipStation/Docs.js
+++ s/ShipStation/Docs.js
@@ -42,6 +42,7 @@ GM_addStyle([
 // 2026-08-25
 // language=CSS
 GM_addStyle([
+	`[data-component-name='Navbar/Navbar']` // 2026-08-25
 ]
 	 // language=Javascript
 	.join(',') + '{display: none !important;}')
