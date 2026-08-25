4ba70ad6f553519f6a0aa9f2112bb857c8feba8d
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-25 07:24:11 +0100

    https://github.com/dmitrii-fediuk/vm/issues/137

--- s/ShipStation/Blog.js
+++ s/ShipStation/Blog.js
@@ -42,6 +42,7 @@ GM_addStyle([
 GM_addStyle([
 	`#main-content > :not(:has(.post-container))` // 2026-08-25
 	,`.author-box` // 2026-08-25
+	,`.entry-content > :not(.entry-meta)` // 2026-08-25
 	,`.post-content ~ *` // 2026-08-25
 	,`[class*=sticky]` // 2026-08-25
 	,`body > :not(#main-content)` // 2026-08-25
