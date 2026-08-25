8accf2ec00a39ea95f01fe55deb7cea28c959c48
Dmitrii Fediuk <dfediuk@gmail.com>
2026-08-25 06:59:16 +0100

    https://github.com/dmitrii-fediuk/vm/issues/137

--- s/ShipStation/Help.js
+++ s/ShipStation/Help.js
@@ -414,7 +414,7 @@ GM_addStyle([
 );
 // 2026-08-25
 // language=CSS
-//GM_addStyle(`body {margin: .5rem !important;}`);
+GM_addStyle(`body {margin: .5rem !important;}`);
 // 2026-08-25
 // language=CSS
 GM_addStyle(`h2 {margin: .5rem 0 !important;}`);
