f5863e79b7da946873e8212641a5616953d1f199
Dmitrii Fediuk <dfediuk@gmail.com>
2026-09-09 22:01:26 +0100

    https://github.com/dmitrii-fediuk/vm/issues/52

--- s/Upwork/project/list/normal/.js
+++ s/Upwork/project/list/normal/.js
@@ -460,7 +460,7 @@ else {
 			filters.every(f => f(a)) ? format(a) : a.style.display = 'none'
 		);
 	})();
-	setTimeout(() => {process(document.querySelector('.card-list-container'));}, 50);
+	//setTimeout(() => {process(document.querySelector('.card-list-container'));}, 50);
 	// 2025-03-18 https://chatgpt.com/c/67d98719-3eec-8003-9df4-844aa046c43b
 	(new MutationObserver(mm => {
 		// 2025-03-18 https://grok.com/chat/293ac71e-03ab-475a-ab7e-0030d1035357
@@ -476,7 +476,7 @@ else {
 				process(n);
 			}
 		});
-	})).observe(document.querySelector('.jobs-grid-container'), {
+	})).observe(document.body, {
 		// 2025-03-18 https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/observe#attributes
 		attributes: false
 		// 2025-03-18 https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/observe#childlist
